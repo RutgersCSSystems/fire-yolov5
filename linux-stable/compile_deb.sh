f59f812ce7cbc897a57dd5a3b7047aaac903dd50
sudarsun <sudarsun.kannan@gmail.com>
2018-06-21 16:51:23 -0500

    adding kernel compilation scripts

--- linux-stable/compile_deb.sh
+++ linux-stable/compile_deb.sh
@@ -3,6 +3,7 @@
 sudo apt-get install libdpkg-dev
 export CONCURRENCY_LEVEL=40
 export CONCURRENCYLEVEL=40
+touch REPORTING-BUGS
 fakeroot make-kpkg --initrd kernel-image kernel-headers
 dpkg -i ../*image*.deb ../*header*.deb
 exit
