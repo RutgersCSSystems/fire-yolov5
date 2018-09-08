a743d41ab515f4bec2ec7ba88b01bf380aa2acaa
sudarsun <sudarsun.kannan@gmail.com>
2018-09-08 14:53:06 -0600

    script additions for appbench

---
+++ scripts/compile_deb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+sudo apt-get install libdpkg-dev kernel-package
+export CONCURRENCY_LEVEL=32
+export CONCURRENCYLEVEL=32
+touch REPORTING-BUGS
+mv .config .config_back
+make distclean
+make menuconfig
+#fakeroot make-kpkg clean
+fakeroot make-kpkg --initrd kernel-image kernel-headers
+dpkg -i ../*image*.deb ../*header*.deb
+exit
---
+++ scripts/set_appbench.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Set variable, setup packages and generate data
+$SCRIPTS/compile_sharedlib.sh
+git clone https://github.com/SudarsunKannan/appbench
+cd $APPBENCH
+source $APPBENCH/setvars.sh
+$APPBENCH/setup.sh
+$APPBENCH/compile_all.sh
+
+#Compile Linux Kernel
+#$SCRIPTS/compile_deb.sh
+
+#Changing bandwidth of a NUMA node
+$APPBENCH/install_quartz.sh
+$APPBENCH/throttle.sh
+
+INSTALL_SYSTEM_LIBS(){
+sudo apt-get install -y git
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y python3-software-properties
+sudo apt-get install -y python-software-properties
+sudo apt-get install -y unzip
+sudo apt-get install -y python-setuptools python-dev build-essential
+sudo easy_install pip
+sudo apt-get install -y numactl
+sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y libnuma-dev
+sudo apt-get install -y cmake
+sudo apt-get install -y build-essential
+sudo apt-get install -y maven
+sudo apt-get install -y fio
+sudo apt-get install -y libbfio-dev
+sudo apt-get install -y libboost-dev
+sudo apt-get install -y libboost-thread-dev
+sudo apt-get install -y libboost-system-dev
+sudo apt-get install -y libboost-program-options-dev
+sudo apt-get install -y libconfig-dev
+sudo apt-get install -y uthash-dev
+sudo apt-get install -y cscope
+sudo apt-get install -y msr-tools
+sudo apt-get install -y msrtool
+sudo pip install psutil
+#sudo pip install thrift_compiler
+#INSTALL_JAVA
+sudo apt-get -y install build-essential
+sudo apt-get -y install libssl-dev
+}
+
+INSTALL_SYSTEM_LIBS
+
+
