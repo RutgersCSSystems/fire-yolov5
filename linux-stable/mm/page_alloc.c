be2768d9f8dabdff6afa2d18065bdac3ff2d2da7
sudarsun <sudarsun.kannan@gmail.com>
2018-09-16 17:18:20 -0600

    changes

--- linux-stable/mm/page_alloc.c
+++ linux-stable/mm/page_alloc.c
@@ -4329,6 +4329,43 @@ static inline bool prepare_alloc_pages(gfp_t gfp_mask, unsigned int order,
 	return true;
 }
 
+#ifdef _ENABLE_HETERO
+static inline bool prepare_alloc_pages_hetero(gfp_t gfp_mask, unsigned int order,
+                int preferred_nid, nodemask_t *nodemask,
+                struct alloc_context *ac, gfp_t *alloc_mask,
+                unsigned int *alloc_flags)
+{
+        ac->high_zoneidx = gfp_zone(gfp_mask);
+        ac->zonelist = node_zonelist(preferred_nid, gfp_mask);
+        ac->nodemask = nodemask;
+        ac->migratetype = gfpflags_to_migratetype(gfp_mask);
+
+        if (cpusets_enabled()) {
+         
+                printk(KERN_ALERT "%s : %d  \n", __func__, __LINE__);
+
+                *alloc_mask |= __GFP_HARDWALL;
+                if (!ac->nodemask)
+                        ac->nodemask = &cpuset_current_mems_allowed;
+                else
+                        *alloc_flags |= ALLOC_CPUSET;
+        }
+
+        fs_reclaim_acquire(gfp_mask);
+        fs_reclaim_release(gfp_mask);
+
+        might_sleep_if(gfp_mask & __GFP_DIRECT_RECLAIM);
+
+        if (should_fail_alloc_page(gfp_mask, order))
+                return false;
+
+        if (IS_ENABLED(CONFIG_CMA) && ac->migratetype == MIGRATE_MOVABLE)
+                *alloc_flags |= ALLOC_CMA;
+
+        return true;
+}
+#endif
+
 /* Determine whether to spread dirty pages and what the first usable zone */
 static inline void finalise_ac(gfp_t gfp_mask,
 		unsigned int order, struct alloc_context *ac)
@@ -4415,7 +4452,7 @@ __alloc_pages_nodemask_hetero(gfp_t gfp_mask, unsigned int order, int preferred_
 
         preferred_nid = NUMA_HETERO_NODE;
 
-	if (!prepare_alloc_pages(gfp_mask, order, preferred_nid, nodemask, &ac, &alloc_mask, &alloc_flags))
+	if (!prepare_alloc_pages_hetero(gfp_mask, order, preferred_nid, nodemask, &ac, &alloc_mask, &alloc_flags))
 		return NULL;
 
 	finalise_ac(gfp_mask, order, &ac);
@@ -4445,8 +4482,10 @@ __alloc_pages_nodemask_hetero(gfp_t gfp_mask, unsigned int order, int preferred_
 
 	page = __alloc_pages_slowpath(alloc_mask, order, &ac);
         if(!page) {
-	        printk(KERN_ALERT "__alloc_pages_nodemask_hetero failed \n");
+                printk(KERN_ALERT "%s : %d  \n", __func__, __LINE__);
                 return __alloc_pages_nodemask_hetero(gfp_mask, order, preferred_nid, nodemask);
+        }else {
+                printk(KERN_ALERT "%s : %d Node: %d \n", __func__, __LINE__,page_to_nid(page));
         }
 out:
 	if (memcg_kmem_enabled() && (gfp_mask & __GFP_ACCOUNT) && page &&
