0b3a50e334ebd59a661bbb972a95bc02c32604fb
sudarsun <sudarsun.kannan@gmail.com>
2019-01-20 15:29:37 -0700

    test

--- scripts/git_configure.sh
+++ scripts/git_configure.sh
@@ -2,5 +2,8 @@
 "Configure scripts to repo"
 git config credential.helper store
 git config --global user.name "sudarsun"
+echo "a" > test.txt
+git add test.txt
 git commit -am "test"
+git rm test.txt
 git push origin cleaned
