e8479440c22f9a4dd5ab873993344b93c4ee7d5e
sudarsun <sudarsun.kannan@gmail.com>
2019-05-08 13:15:42 -0600

    script changes

--- scripts/mount_ext4ramdisk.sh
+++ scripts/mount_ext4ramdisk.sh
@@ -2,11 +2,13 @@
 #script to create and mount a pmemdir
 #requires size as input
 #xfs
+rm -rf $APPBENCH/shared_data
+sudo umount /mnt/ext4ramdisk
 sudo mkdir /mnt/ramdisk
 sudo mount -t ramfs ramfs /mnt/ramdisk
-sudo dd if=/dev/zero of=/mnt/ramdisk/ext4.image bs=1M count=16384
-mkfs.ext4 /mnt/ramdisk/ext4.image
-mkdir /mnt/ext4ramdisk
-mount -o loop /mnt/ramdisk/ext4.image /mnt/ext4ramdisk
+sudo dd if=/dev/zero of=/mnt/ramdisk/ext4.image bs=1M count=49152
+sudo mkfs.ext4 /mnt/ramdisk/ext4.image
+sudo mkdir /mnt/ext4ramdisk
+sudo mount -o loop /mnt/ramdisk/ext4.image /mnt/ext4ramdisk
 sudo chown -R $USER /mnt/ext4ramdisk
-ln -s /mnt/ext4ramdisk $APPBENCH/shared_data
+sudo ln -s /mnt/ext4ramdisk $APPBENCH/shared_data
