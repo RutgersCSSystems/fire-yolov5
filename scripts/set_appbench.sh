1bfe74c2b3a4ceb7b969a093187073ebb245a9db
sudarsun <sudarsun.kannan@gmail.com>
2019-10-20 09:00:02 -0600

    adding spark installation

--- scripts/set_appbench.sh
+++ scripts/set_appbench.sh
@@ -2,60 +2,46 @@
 set -x
 
 INSTALL_SYSTEM_LIBS(){
-sudo apt-get install -y git
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y python3-software-properties
-sudo apt-get install -y python-software-properties
-sudo apt-get install -y unzip
-sudo apt-get install -y python-setuptools python-dev build-essential
-sudo easy_install pip
-sudo apt-get install -y numactl
-sudo apt-get install -y libsqlite3-dev
-sudo apt-get install -y libnuma-dev
-sudo apt-get install -y cmake
-sudo apt-get install -y build-essential
-sudo apt-get install -y maven
-sudo apt-get install -y fio
-sudo apt-get install -y libbfio-dev
-sudo apt-get install -y libboost-dev
-sudo apt-get install -y libboost-thread-dev
-sudo apt-get install -y libboost-system-dev
-sudo apt-get install -y libboost-program-options-dev
-sudo apt-get install -y libconfig-dev
-sudo apt-get install -y uthash-dev
-sudo apt-get install -y cscope
-sudo apt-get install -y msr-tools
-sudo apt-get install -y msrtool
-sudo pip install psutil
-#sudo pip install thrift_compiler
-#INSTALL_JAVA
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk
-sudo apt-get -y install build-essential
-sudo apt-get -y install libssl-dev
-sudo apt-get install -y libgflags-dev
-sudo apt-get install -y zlib1g-dev
-sudo apt-get install -y libbz2-dev
-sudo apt-get install -y libevent-dev
-sudo apt-get install -y systemd
-#sudo apt-get install memcached
-sudo apt-get install libaio*
-sudo apt-get install software-properties-common
-}
-
-INSTALL_SPARK(){
-sudo service docker stop
-sudo apt-get -y remove docker docker.io
-sudo rm -rf /var/lib/docker $APPBENCH/docker
-mkdir $APPBENCH/docker
-sudo apt-get -y install docker docker.io
-sudo service docker stop
-sudo cp scripts/docker_new.service /lib/systemd/system/docker.service
-sudo systemctl daemon-reload
-sudo service docker start
-sudo docker pull cloudsuite/graph-analytics
-sudo docker pull cloudsuite/twitter-dataset-graph
+	sudo apt-get install -y git
+	sudo apt-get install -y software-properties-common
+	sudo apt-get install -y python3-software-properties
+	sudo apt-get install -y python-software-properties
+	sudo apt-get install -y unzip
+	sudo apt-get install -y python-setuptools python-dev build-essential
+	sudo easy_install -y pip
+	sudo apt-get install -y numactl
+	sudo apt-get install -y libsqlite3-dev
+	sudo apt-get install -y libnuma-dev
+	sudo apt-get install -y cmake
+	sudo apt-get install -y build-essential
+	sudo apt-get install -y maven
+	sudo apt-get install -y fio
+	sudo apt-get install -y libbfio-dev
+	sudo apt-get install -y libboost-dev
+	sudo apt-get install -y libboost-thread-dev
+	sudo apt-get install -y libboost-system-dev
+	sudo apt-get install -y libboost-program-options-dev
+	sudo apt-get install -y libconfig-dev
+	sudo apt-get install -y uthash-dev
+	sudo apt-get install -y cscope
+	sudo apt-get install -y msr-tools
+	sudo apt-get install -y msrtool
+	sudo pip install -y psutil
+	#sudo pip install thrift_compiler
+	#INSTALL_JAVA
+	sudo add-apt-repository ppa:webupd8team/java
+	sudo apt-get update
+	sudo apt-get install -y openjdk-8-jdk
+	sudo apt-get install -y build-essential
+	sudo apt-get install -y libssl-dev
+	sudo apt-get install -y libgflags-dev
+	sudo apt-get install -y zlib1g-dev
+	sudo apt-get install -y libbz2-dev
+	sudo apt-get install -y libevent-dev
+	sudo apt-get install -y systemd
+	#sudo apt-get install memcached
+	sudo apt-get install -y libaio*
+	sudo apt-get install -y software-properties-common
 }
 
 INSTALL_CMAKE(){
@@ -90,61 +76,19 @@ INSTALL_MYSQL() {
 	sudo systemctl start mysql
 }
 
-ADD_SPARK_TO_BASHRC() {
-	echo "export SPARK_HOME=/users/skannan/ssd/NVM/appbench/apps/spark" &>> ~/.bashrc
-	echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" &>> ~/.bashrc
-	echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre" &>> ~/.bashrc
-	echo "export HADOOP_HOME=$SPARK_HOME/hadoop-3.2.1" &>> ~/.bashrc
-	echo "export HADOOP_INSTALL=$HADOOP_HOME" &>> ~/.bashrc
-	echo "export HADOOP_MAPRED_HOME=$HADOOP_HOME" &>> ~/.bashrc
-	echo "export HADOOP_COMMON_HOME=$HADOOP_HOME" &>> ~/.bashrc
-	echo "export HADOOP_HDFS_HOME=$HADOOP_HOME" &>> ~/.bashrc
-	echo "export YARN_HOME=$HADOOP_HOME" &>> ~/.bashrc
-	echo "export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native" &>> ~/.bashrc
-	echo "export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin" &>> ~/.bashrc
-	echo "export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"" &>> ~/.bashrc
-	ssh-keygen
-	cat ~/.ssh/id_rsa.pub &>> ~/.ssh/authorized_keys
-}
 
-INSTALL_SPARK_HIBENCH(){
-	cd $APPBENCH/apps
-	SPARKFILE=spark-2.4.4-bin-hadoop2.7.tgz
-	SPARKDIR=$APPBENCH/apps/spark
-	HIBENCHDIR=$SPARKDIR/HiBench
-        SPARKFILES=$APPBENCH/apps/spark_files
-	HADOOP="hadoop-3.2.1"
-	HADOOP_DIR=$SPARKDIR/$HADOOP
-
-       ADD_SPARK_TO_BASHRC
-
-	# Check if Spark file exists?
-	if [ -f $SPARKFILE ]; then
-	  	  echo "$SPARKFILE exist"
-	else 
-		wget https://www.apache.org/dist/spark/spark-2.4.4/$SPARKFILE
-		tar -xvzf $SPARKFILE
-	fi
-
-	if [ -d $SPARKDIR ]; then
-		echo "$SPARKDIR exist"
-	else
-		mv spark-2.4.4* $SPARKDIR
-	fi
-	cd $SPARKDIR
-
-	if [ -f $HADOOP".tar.gz" ]; then
-  	  echo "$HADOOP".tar.gz" exist"
-	else 
-	    wget http://apache.mirrors.pair.com/hadoop/common/$HADOOP/$HADOOP".tar.gz"
-	    tar -xvzf $HADOOP".tar.gz"
-	fi
-	
-        git clone https://github.com/Intel-bigdata/HiBench
-        cd $HIBENCHDIR
-	mvn -Dspark=2.1 -Dscala=2.11 clean package
-	cp $SPARKFILES/$HADOOP/etc/* $HADOOP_DIR/etc/
-        cp $SPARKFILES/HiBench/conf/* $HIBENCHDIR/conf/ 
+INSTALL_SPARK_DOCKER_CLOUDSUITE(){
+	sudo service docker stop
+	sudo apt-get -y remove docker docker.io
+	sudo rm -rf /var/lib/docker $APPBENCH/docker
+	mkdir $APPBENCH/docker
+	sudo apt-get -y install docker docker.io
+	sudo service docker stop
+	sudo cp scripts/docker_new.service /lib/systemd/system/docker.service
+	sudo systemctl daemon-reload
+	sudo service docker start
+	sudo docker pull cloudsuite/graph-analytics
+	sudo docker pull cloudsuite/twitter-dataset-graph
 }
 
 
@@ -183,19 +127,19 @@ INSTALL_GFLAGS(){
 
 #Get Other Apps not in out Repo
 GETAPPS(){
-mkdir $APPBENCH
-cd $APPBENCH
-git clone https://github.com/SudarsunKannan/leveldb
-mkdir $APPBENCH/apps
-cd $APPBENCH/apps
-git clone https://github.com/SudarsunKannan/fio
-cd $APPBENCH/apps
-git clone https://github.com/memcached/memcached.git
-INSTALL_CASSANDRA
+	mkdir $APPBENCH
+	cd $APPBENCH
+	git clone https://github.com/SudarsunKannan/leveldb
+	mkdir $APPBENCH/apps
+	cd $APPBENCH/apps
+	git clone https://github.com/SudarsunKannan/fio
+	cd $APPBENCH/apps
+	git clone https://github.com/memcached/memcached.git
+	INSTALL_CASSANDRA
 }
 
 INSTALL_SYSTEM_LIBS
-INSTALL_SPARK_HIBENCH
+scripts/set_spark.sh
 exit
 
 INSTALL_CMAKE
