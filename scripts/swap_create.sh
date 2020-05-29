717fbba2dd80a4a84ef15a159d75a647ba60a0a1
Shaleen Garg <shaleengarg.in@gmail.com>
2020-05-29 15:29:20 -0600

    swap scripts

--- scripts/swap_create.sh
+++ scripts/swap_create.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-sudo fallocate -l 2G /users/kannan11/ssd/swapfile
-sudo dd if=/dev/zero of=/users/kannan11/ssd/swapfile bs=1024 count=1048576
-sudo chmod 600 /users/kannan11/ssd/swapfile
-sudo mkswap /users/kannan11/ssd/swapfile
-sudo swapon /users/kannan11/ssd/swapfile
+DIR=$1
+DEFAULTSWAP="/dev/sda3"
+sudo fallocate -l 2G $DIR/swapfile
+sudo dd if=/dev/zero of=$DIR/swapfile bs=1024 count=1048576
+sudo chmod 600 $DIR/swapfile
+sudo mkswap $DIR/swapfile
+sudo swapoff $DEFAULTSWAP
+sudo swapon -s
