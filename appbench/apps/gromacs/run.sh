8ecef7664114ecb9dd736b4c41a816f013c2a5aa
Shaleen Garg <shaleengarg.in@gmail.com>
2020-06-08 11:46:19 -0500

    gromacs run

--- appbench/apps/gromacs/run.sh
+++ appbench/apps/gromacs/run.sh
@@ -7,4 +7,13 @@ NPROC=36
 #gmx pdb2gmx -f water.gro -o water.gro -p water.top -n water.ndx
 #gmx grompp -v -f water_MD.mdp -c water.gro -p water.top -o run_water
 
-/usr/bin/time -v gmx mdrun -ntmpi $NPROC -ntomp 1 -nt $NPROC -s run_water.tpr -o -x -deffnm md_water
+./clean_out.sh
+
+LD_PRELOAD=/usr/lib/libmigration.so /usr/bin/time -v gmx mdrun -ntmpi $NPROC -ntomp 1 -nt $NPROC -s run_water.tpr -o -x -deffnm md_water &
+
+/users/shaleen/ssd/NVM/appbench/apps/NPB3.4/NPB3.4-MPI/scripts/readdmesg.py init
+while :
+do
+	/users/shaleen/ssd/NVM/appbench/apps/NPB3.4/NPB3.4-MPI/scripts/readdmesg.py readfrom Cummulate.csv
+	sleep 1
+done
