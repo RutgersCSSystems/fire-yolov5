10b0700776222bc6382aea541a1825ce104b8a3c
shaleengarg <shaleengarg.in@gmail.com>
2020-06-11 12:37:42 -0400

    gromacs

---
+++ scripts/install_gromacs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir $HOME/ssd/Downloads
+
+pushd $HOME/ssd/Downloads
+
+wget http://ftp.gromacs.org/pub/gromacs/gromacs-2019.tar.gz
+
+tar -xvf gromacs-2019.tar.gz
+
+cd gromacs-2019
+
+mkdir build
+
+cd build
+
+cmake .. -DGMX_BUILD_OWN_FFTW=on -DGMX_MPI=on -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx
+
+make -j`nproc`
+
+sudo make install
+
+popd
