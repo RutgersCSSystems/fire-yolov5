01d2016c53e4c023d88f4c6a764222e024506cde
shaleen <shaleen@node-0.doit.lsm-pg0.utah.cloudlab.us>
2020-06-13 16:45:56 -0600

    mpich install

---
+++ scripts/install_mpich.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+if [ -z "$PREFIX" ]; then
+    kernel_error "PREFIX environment variable not defined. Have you ran setvars?"
+    exit 1
+fi
+
+LINK=http://www.mpich.org/static/downloads/3.2.1/mpich-3.2.1.tar.gz
+DOWNLOAD_LOC=$HOME/ssd/Downloads
+INSTALL_LOC=$PREFIX
+
+sudo apt-get install gfortran libpthread-stubs0-dev -y
+
+stat $DOWNLOAD_LOC
+if [ $? -ne 0 ]
+then
+    mkdir $DOWLOAD_LOC
+fi
+
+set -x
+
+cd $DOWNLOAD_LOC
+wget $LINK
+tar -xvf mpich-3.2.1.tar.gz
+
+cd mpich-3.2.1
+
+#./configure --disable-fortran --prefix=$INSTALL_LOC --enable-g=yes --enable-debuginfo
+./configure --enable-g=yes --enable-debuginfo
+
+make -j`nproc`
+sudo make install
+set +x
