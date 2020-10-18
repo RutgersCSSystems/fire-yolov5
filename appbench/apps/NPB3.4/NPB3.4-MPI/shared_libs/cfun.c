f860882cd04f3776f5ae10eed6b451b1e520a099
Shaleen Garg <shaleengarg.in@gmail.com>
2020-10-17 22:24:45 -0500

    close + prints

--- appbench/apps/NPB3.4/NPB3.4-MPI/shared_libs/cfun.c
+++ appbench/apps/NPB3.4/NPB3.4-MPI/shared_libs/cfun.c
@@ -12,9 +12,13 @@ typedef size_t (*real_fread_t)(void *, size_t, size_t,FILE *);
 typedef ssize_t (*real_write_t)(int, const void *, size_t);
 typedef size_t (*real_fwrite_t)(const void *, size_t, size_t,FILE *);
 typedef int (*real_fclose_t)(FILE *);
+typedef int (*real_close_t)(int);
 
 size_t real_fclose(FILE *stream){
   return ((real_fclose_t)dlsym(RTLD_NEXT, "fclose"))(stream);
+}
+size_t real_close(int fd){
+  return ((real_close_t)dlsym(RTLD_NEXT, "close"))(fd);
 }
 size_t real_fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
   return ((real_fread_t)dlsym(RTLD_NEXT, "fread"))(ptr, size, nmemb, stream);
@@ -36,6 +40,11 @@ int fclose(FILE *stream){
 	posix_fadvise(fd, 0, 0, POSIX_FADV_DONTNEED);
 
 	return real_fclose(stream);
+}
+
+int close(int fd){
+	//printf("File close detected\n");
+	return real_close(fd);
 }
 
 size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream){
@@ -61,6 +70,7 @@ size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream){
 ssize_t write(int fd, const void *data, size_t size) {
   ssize_t amount_written;
 
+  //printf("write Detected\n");
   // Perform the actual system call
   amount_written = real_write(fd, data, size);
 
@@ -84,7 +94,7 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
 
   //printf("fread Detected\n");
   int fd = fileno(stream);
-  posix_fadvise(fd, 0, 0, POSIX_FADV_WILLNEED);
+  //posix_fadvise(fd, 0, 0, POSIX_FADV_WILLNEED);
 
   // Behave just like the regular syscall would
   return amount_read;
@@ -97,7 +107,7 @@ ssize_t read(int fd, void *data, size_t size) {
   amount_read = real_read(fd, data, size);
 
   //printf("read Detected\n");
-  posix_fadvise(fd, 0, 0, POSIX_FADV_WILLNEED);
+  //posix_fadvise(fd, 0, 0, POSIX_FADV_WILLNEED);
 
   // Behave just like the regular syscall would
   return amount_read;
