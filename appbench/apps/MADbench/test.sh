c8a2594f91854797a8e2bc716979e6a9827949eb
Shaleen Garg <shaleengarg.in@gmail.com>
2020-10-30 08:30:29 +0000

    MAD testing

--- appbench/apps/MADbench/test.sh
+++ appbench/apps/MADbench/test.sh
@@ -1,13 +1,19 @@
 NPROC=16
 #APPPREFIX="numactl --membind=0"
 
-##export LD_PRELOAD=/usr/lib/libmigration.so 
-export LD_PRELOAD=/usr/lib/libcfun.so 
+
+rm -rf files/
+./clear_cache.sh
+dmesg -c
+
+#export LD_PRELOAD=/usr/lib/libmigration.so
+#export LD_PRELOAD="/usr/lib/libcfun.so:/usr/lib/libmigration.so"
+export LD_PRELOAD="/usr/lib/libcfun.so"
 
 export IOMODE=SYNC
 export FILETYPE=UNIQUE
 export IOMETHOD=POSIX
 
-mpiexec -n $NPROC ./MADbench2_io 2400 140 1 8 8 4 4
+/usr/bin/time -v mpiexec -n $NPROC ./MADbench2_io 1400 140 1 8 8 4 4
 #mpiexec -n $NPROC ./MADbench2_io 400 140 1 8 8 1 1
 #$APPPREFIX /usr/bin/time -v mpiexec -n $NPROC ./MADbench2.x 2400 140 1 8 8 4 4
