80f354c7f2882a8485f62990258a9234a3b192da
Shaleen Garg <shaleengarg.in@gmail.com>
2020-11-11 06:25:22 -0600

    MADBench test file

--- appbench/apps/MADbench/test.sh
+++ appbench/apps/MADbench/test.sh
@@ -8,12 +8,12 @@ dmesg -c
 
 #export LD_PRELOAD=/usr/lib/libmigration.so
 #export LD_PRELOAD="/usr/lib/libcfun.so:/usr/lib/libmigration.so"
-export LD_PRELOAD="/usr/lib/libcfun.so"
+export LD_PRELOAD="/usr/lib/libpredictor.so"
 
 export IOMODE=SYNC
 export FILETYPE=UNIQUE
 export IOMETHOD=POSIX
 
-/usr/bin/time -v mpiexec -n $NPROC ./MADbench2_io 1400 140 1 8 8 4 4
+strace /usr/bin/time -v mpiexec -n $NPROC ./MADbench2_io 1400 140 1 8 8 4 4
 #mpiexec -n $NPROC ./MADbench2_io 400 140 1 8 8 1 1
 #$APPPREFIX /usr/bin/time -v mpiexec -n $NPROC ./MADbench2.x 2400 140 1 8 8 4 4
