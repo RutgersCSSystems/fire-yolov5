bfc0f756a23d92b800b618270ce3f1ef22c9e199
sudarsun <youremail.com>
2021-03-16 16:27:18 -0500

    adding master runapps.sh that runs all applications

--- scripts/run/runapps.sh
+++ scripts/run/runapps.sh
@@ -2,31 +2,25 @@
 set -x
 
 RUNNOW=1
-RUNSCRIPT=run.sh
+RUNSCRIPT=runexp_variation_sudarsun.sh
 mkdir $OUTPUTDIR
 sudo dmesg -c &> del.txt
 
 USAGE(){
-echo "./app \$maxhotpage \$BW \$outputdir \$app"
+  echo "echo some content on how run"
 }
 
 RUNAPP(){
-  #rm $OUTPUTDIR/$APP
-  FLAGPATH=$NVMBASE"/flags/"$APP
-  let value=`cat "$FLAGPATH"`
-  if [ $value == 0 ]; then
-	  echo "$APP NOW RUNNING"
-	  cd $APPBASE
-	  $APPBASE/$RUNSCRIPT $RUNNOW $OUTPUTDIR/$APP &> $OUTPUTDIR/$APP
-	  echo "******************"  &>> $OUTPUTDIR/$APP
-	  echo "KERNEL  DMESG"  &>> $OUTPUTDIR/$APP
-	  echo "******************"  &>> $OUTPUTDIR/$APP 	
-	  echo "  "  &>> $OUTPUTDIR/$APP
-	  sudo dmesg -c &>> $OUTPUTDIR/$APP
-	  echo 1 > $FLAGPATH
-  else
-	  echo "$APP ALREADY RUN"	
-  fi
+
+  echo "$APP NOW RUNNING"
+  cd $APPBASE
+  $APPBASE/$RUNSCRIPT $RUNNOW #$OUTPUTDIR/$APP &> $OUTPUTDIR/$APP
+  #echo "******************"  &>> $OUTPUTDIR/$APP
+  #echo "KERNEL  DMESG"  &>> $OUTPUTDIR/$APP
+  #echo "******************"  &>> $OUTPUTDIR/$APP 	
+  #echo "  "  &>> $OUTPUTDIR/$APP
+  #sudo dmesg -c &>> $OUTPUTDIR/$APP
+  #echo 1 > $FLAGPATH
 }
 
 
@@ -45,77 +39,34 @@ hupexit() {
 trap hupexit HUP
 trap intexit INT
 
-#if [ -z "$1" ]
-# then	
-#  USAGE 
-#  exit
-#fi
-
-#$NVMBASE/scripts/copy_data.sh
+if [ -z "$1" ]  then	
+  USAGE 
+  exit
+fi
 
 if [ -z "$4" ]
   then
-        APPBASE=$APPBENCH/apps/filebench
-        APP=filebench
+        APPBASE=$APPBENCH/apps/ior/benchmarks
+        APP=ior
         RUNAPP
-	$NVMBASE/scripts/reset.sh
-	exit	
 
-	APPBASE=$APPBENCH/redis-3.0.0/src
-	APP=redis
+	APPBASE=$APPBENCH/apps/strided_MADbench
+	APP=MADbench2_io
 	echo "running $APP..."
 	RUNAPP
 	$NVMBASE/scripts/reset.sh
-
-	APPBASE=$APPBENCH/apps/rocksdb
-	APP=db_bench
-	RUNAPP
-	$NVMBASE/scripts/reset.sh
-	exit
-
-	APPBASE=$APPBENCH/apps/memcached_client
-	APP=memcached
-	RUNAPP
-	export LD_PRELOAD=$SHARED_LIBS/construct/libmigration.so
-	/bin/ls
-	export LD_PRELOAD=""
-        $NVMBASE/scripts/reset.sh
-        exit
-
-
-
-
-	
-	APPBASE=$APPBENCH/apps/leveldb
-	APP=leveldb
-	echo "running $APP..."
-	RUNAPP
-
-
-
-	
+fi
 	#APPBASE=$APPBENCH/apps/fio
 	#APP=fio
 	#echo "running $APP ..."
 	#RUNAPP
 
-	APPBASE=$APPBENCH/graphchi
-	APP=graphchi
-	RUNAPP
-	# We need data files
-	$SCRIPTS/createdata.sh
-	rm $SHARED_DATA/com-orkut.ungraph.txt.*
-
-	APPBASE=$APPBENCH/Metis
-	APP=Metis
-	$SCRIPTS/createdata.sh
-	RUNAPP
-
-	#APPBASE=$APPBENCH/memcached/memtier_benchmark
-	#APP=memcached
-	#echo "running $APP ..."
+	#APPBASE=$APPBENCH/graphchi
+	#APP=graphchi
 	#RUNAPP
-	#exit
+	# We need data files
+	#$SCRIPTS/createdata.sh
+	#rm $SHARED_DATA/com-orkut.ungraph.txt.*
 
 	#RUNSCRIPT="runfcreate.sh"
         #APP=fcreate
@@ -132,10 +83,5 @@ if [ -z "$4" ]
 	#scp -r $HOSTIP:$SHARED_DATA*.ini $APPBASE
         #cp $APPBASE/*.ini $SHARED_DATA
 	#RUNAPP
-
-fi
-
-finish:
-$NVMBASE/scripts/reset.sh
 #currentDate=`date +"%D %T"`
 set +x
