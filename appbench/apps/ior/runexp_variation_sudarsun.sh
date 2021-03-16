7a81aa2d9adc4879681979c1cadc69318db8ca94
sudarsun <youremail.com>
2021-03-16 16:51:27 -0500

    changing outputdir of IOR

--- appbench/apps/ior/runexp_variation_sudarsun.sh
+++ appbench/apps/ior/runexp_variation_sudarsun.sh
@@ -3,22 +3,34 @@
 
 ##prefetch window multiple factor 1, 2, 4
 ##grep the elapsed time, file faults, minor faults, system time, user time
-
+APP="IOR"
 APPDIR=$PWD
-RESULTS_FOLDER=results-sensitivity-oldlinux-16
-
-mkdir $RESULTS_FOLDER
+RESULTS_FOLDER=$OUTPUTDIR/$APP/results-sensitivity-sudarsun/noprep-threading
+mkdir -p $RESULTS_FOLDER
 
 cd $APPDIR
 
 declare -a apparr=("ior")
 declare -a predict=("0" "1")
-declare -a thrdarr=("16")
+declare -a thrdarr=("16" "32")
+
 declare -a transfersizearr=("8192" "16384") #transfer size
 declare -a blockprodarr=("100000" "150000" "200000") #blocksize = transfersize*blockprod
 declare -a segmentarr=("1" "256" "1024" "2048") #segmentsize
+
+declare -a thrdarr=("8")
+declare -a transfersizearr=("1048576") #transfer size
+declare -a blockprodarr=("1000") #blocksize = transfersize*blockprod
+declare -a segmentarr=("256") #segmentsize
+declare -a predict=("0" "1")
 #sizeofprefetch = prefetchwindow * readsize
-declare -a prefetchwindow=("1" "2" "4")
+declare -a prefetchwindow=("1" "2")
+#declare -a prefetchwindow=("8")
+
+
+
+#reduce the dirty files aggressively
+$ENVPATH/set_disk_dirty.sh
 
 #APPPREFIX="numactl --membind=0"
 APPPREFIX=""
@@ -28,6 +40,15 @@ REFRESH() {
 	$NVMBASE/scripts/compile-install/clear_cache.sh
 	sudo sh -c "dmesg --clear" ##clear dmesg
 	sleep 2
+}
+
+SYNCFILES() {
+
+	OUTPUT=$1
+	wait; sync
+	echo "*******************DMESG OUTPUT******************" >> $OUTPUT
+	dmesg | grep -v -F "systemd-journald" >> $OUTPUT
+	wait; sync
 }
 
 #Here is where we run the application
@@ -44,12 +65,27 @@ RUNAPP() {
 	BLOCKTIMES=$6
 	BLOCKSIZE=`echo "$TRANSFER * $BLOCKTIMES" | bc`
 	TPREFETCH=$7
+	REORDER="-C"
+	FILEPERPROC="-F"
+	KEEPFILE="-k"
 
-	OUTPUT=$RESULTS_FOLDER/$APP"_PROC-"$NPROC"_PRED-"$PREDICT"_BLKSIZE-"$BLOCKSIZE"_TRANSFERSIZE-"$TRANSFER"_SEGMENTS-"$SEGMENT"_TIMESPFETCH-"$TPREFETCH".out"
+	#Unused
+	MADVICE="--mmap.madv_dont_need" #Currently not used
+	REORDERTASKRAND="-Z" #reorderTasksRandom -- changes task ordering to random ordering for readback
+
+	OUTPUTDIR=$RESULTS_FOLDER/"BLKSIZE-"$BLOCKSIZE
+	mkdir -p $OUTPUTDIR
+
+	OUTPUT=$OUTPUTDIR/$APP"_PROC-"$NPROC"_PRED-"$PREDICT"_BLKSIZE-"$BLOCKSIZE"_TRANSFERSIZE-"$TRANSFER"_SEGMENTS-"$SEGMENT"_TIMESPFETCH-"$TPREFETCH".out"
 
 	echo "********** prepping File **************"
-	echo "mpirun -np $NPROC ior -w -k -e -o $FILENAME -v -b $BLOCKSIZE -t $TRANSFER -s $SEGMENT"
-	mpirun -np $NPROC ior -w -F -k -e -o $FILENAME -v -b $BLOCKSIZE -t $TRANSFER -s $SEGMENT
+
+	PARAMS="-e -o $FILENAME -v -b $BLOCKSIZE -t $TRANSFER -s $SEGMENT $REORDER $FILEPERPROC $KEEPFILE"
+	WRITE=" -w "
+	READ=" -r "
+
+	#echo "mpirun -np $NPROC ior $WRITE $PARAMS"
+	#mpirun -np $NPROC ior $WRITE $PARAMS
 
 	REFRESH
 
@@ -63,21 +99,17 @@ RUNAPP() {
 		export LD_PRELOAD=/usr/lib/libnopred.so
 	fi
 
+	numactl --hardware &> $OUTPUT
+	wait; sync
 
 	if [[ "$APP" == "ior" ]]; then
-		rm -rf $OUTPUT
-		echo "$APPPREFIX mpirun -np $NPROC ior -r -F -o $FILENAME -v -b $BLOCKSIZE -t $TRANSFER -s $SEGMENT"
-		numactl --hard &> $OUTPUT
-		wait; sync
-		$APPPREFIX mpirun -np $NPROC ior -r -F -o $FILENAME -v -b $BLOCKSIZE -t $TRANSFER -s $SEGMENT &>> $OUTPUT
+
+		echo "$APPPREFIX mpirun -np $NPROC $READ $PARAMS"
+		#$APPPREFIX mpirun -np $NPROC ior $READ $PARAMS &>> $OUTPUT
+		$APPPREFIX mpirun -np $NPROC ior $PARAMS &>> $OUTPUT
 		export LD_PRELOAD=""
-
 		REFRESH
-
-		wait; sync
-		echo "*******************DMESG OUTPUT******************" >> $OUTPUT
-		dmesg | grep -v -F "systemd-journald" >> $OUTPUT
-		wait; sync
+		SYNCFILES $OUTPUT
 	fi
 }
 
@@ -98,10 +130,13 @@ do
 					do 
 						for PREFETCHTIMES in "${prefetchwindow[@]}"
 						do 
-
 							RUNAPP $NPROC $APP $PREDICT $SEGMENT $TRANSFERSIZE $BLOCKPROD $PREFETCHTIMES
 							REFRESH
 							rm -rf $FILENAME*
+
+							if [ "$PREDICT" -eq "0" ]; then
+								break;
+							fi
 						done
 					done
 				done
