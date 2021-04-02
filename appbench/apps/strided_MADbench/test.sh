4510619db9d9a7fbf18f363ef8abad50f96fc85c
Shaleen Garg <shaleengarg.in@gmail.com>
2021-04-02 03:52:54 -0500

    test script shared_madbench amplxe checks

--- appbench/apps/strided_MADbench/test.sh
+++ appbench/apps/strided_MADbench/test.sh
@@ -1,30 +1,31 @@
 #APPPREFIX="numactl --membind=0"
 
 
-rm -rf files/
-$NVMBASE/scripts/compile-install/clear_cache.sh
+rm -rf /users/shaleen/ssd/NVM/appbench/apps/strided_MADbench/files/
+/users/shaleen/ssd/NVM/scripts/compile-install/clear_cache.sh
 sudo sh -c "dmesg --clear"
 
 export IOMODE=SYNC
 export FILETYPE=UNIQUE
 export IOMETHOD=POSIX
-RECORD=16777216 # bytes read at once
-#RECORD=4096 # bytes read at once
+#RECORD=16777216 # bytes read at once
+RECORD=4096 # bytes read at once
 #RECORD=1048576 # bytes read at once
 STRIDE=7 # set stride to $STRIDE * RECORD_SIZE
-NPROC=4 ##Num MPI procs
+NPROC=16 ##Num MPI procs
 FLUSH=1 ##flush writes
 
-export LD_PRELOAD="/usr/lib/libcrosslayer.so"
-#export LD_PRELOAD="/usr/lib/libnopred.so"
+#AMPLXE=/opt/intel/vtune_amplifier_2019/bin64/amplxe-cl
+#CONFIG_AMPLXE="-trace-mpi -collect hotspots -k enable-stack-collection=true -k stack-size=0 -k sampling-mode=hw"
+#CONFIG_AMPLXE="-trace-mpi -collect io -k kernel-stack=false -k spdk=true"
+#CONFIG_AMPLXE="-trace-mpi -collect memory-consumption"
+#CONFIG_AMPLXE="-trace-mpi -collect hpc-performance -k enable-stack-collection=true -k stack-size=2147483640 -k collect-affinity=true "
+#CONFIG_AMPLXE="-trace-mpi -collect platform-profiler"
 
-#last two values should multiply to NPROC
+#export LD_PRELOAD="/usr/local/lib/libdarshan.so"
+#export LD_PRELOAD="/usr/lib/libcrosslayer.so"
+export LD_PRELOAD="/usr/lib/libnopred.so"
 
-#/usr/bin/time -v mpiexec -n $NPROC ./MADbench2_io 8192 30 1 8 64 1 1 $RECORD $STRIDE $FLUSH
-TIMESPREFETCH=2 /usr/bin/time -v mpiexec -n $NPROC ./MADbench2_io 8192 5 1 8 64 1 1 $RECORD $STRIDE $FLUSH
-#strace ./MADbench2_io 4096 1 1 8 64 1 1 $RECORD $STRIDE 2> mystrace
-#strace ./MADbench2_io 4096 1 1 8 64 1 1 $RECORD $STRIDE 2> mystrace
-#mpiexec -n $NPROC ./MADbench2_io 400 140 1 8 8 1 1
-#$APPPREFIX /usr/bin/time -v mpiexec -n $NPROC ./MADbench2.x 2400 140 1 8 8 4 4
+TIMESPREFETCH=2 /usr/bin/time -v mpiexec -n $NPROC ./MADbench2_io 16384 10 1 8 64 1 1 $RECORD $STRIDE $FLUSH
 
 export LD_PRELOAD=
