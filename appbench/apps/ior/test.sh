82d02a58569f7382fe42c09f4744f83b98682e99
shaleen <shaleen@node-0.setscripts.lsm-pg0.wisc.cloudlab.us>
2021-04-08 01:34:02 -0500

    testing with vtune

---
+++ appbench/apps/ior/test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#set -x
+
+if [ -z "$NVMBASE" ]; then
+	echo "NVMBASE environment variable not defined. Have you ran setvars?"
+	exit 1
+fi
+
+REFRESH() {
+	/users/shaleen/ssd/NVM/scripts/compile-install/clear_cache.sh
+	sudo sh -c "dmesg --clear" ##clear dmesg
+	sleep 2
+}
+
+FILENAME=test_outfile_ior
+NPROC=8
+TRANSFERSZ=8192 #TRANSFER
+BLOCKPROD=100000 #blocksize = transfersize*blockprod
+NR_SEGMENTS=10 #SEGMENT
+PREFETCH_WIN=2 #TPREFETCH
+
+BLOCKSIZE=`echo "$TRANSFERSZ * $BLOCKPROD" | bc`
+export TIMESPREFETCH=$PREFETCH_WIN
+APPPREFIX="/usr/bin/time -v"
+
+REORDER="-C"
+FILEPERPROC="-F"
+KEEPFILE="-k"
+
+PARAMS="-e -o $FILENAME -v -b $BLOCKSIZE -t $TRANSFERSZ -s $NR_SEGMENTS $FILEPERPROC $KEEPFILE"
+WRITE=" -w "
+READ=" -r "
+
+rm -rf $FILENAME*
+#echo "********** prepping File **************"
+$APPPREFIX mpirun -np $NPROC ior $WRITE $PARAMS
+REFRESH
+
+#echo "********** read workload *************"
+$APPPREFIX mpirun -np $NPROC ior $READ $PARAMS
+
+
+#export LD_PRELOAD="/usr/lib/libcrosslayer.so"
+export LD_PRELOAD="/usr/lib/libnopred.so"
+
