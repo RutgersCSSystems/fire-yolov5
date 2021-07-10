e1c4d53f087ccf94d7098688b98806ef1d847630
shaleen <shaleen@node-0.vanilla.lsm-pg0.wisc.cloudlab.us>
2021-07-10 08:35:30 -0500

    histogram of io perf

---
+++ scripts/compile-install/install_biolatency.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+sudo apt update
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
+echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/iovisor.list
+sudo apt update
+sudo apt-get install bcc-tools libbcc-examples blktrace collectl perf-tools-unstable bcc bpfcc-tools bcftools nmon htop -y
