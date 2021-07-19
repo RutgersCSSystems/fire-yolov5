f342b9007e79ee43d44b3dd8e56fcc2a19f2399e
shaleen <shaleen@node-0.vanilla.lsm-pg0.wisc.cloudlab.us>
2021-07-19 04:55:23 -0500

    testing again

--- appbench/apps/strided_MADbench/oldtest.sh
+++ appbench/apps/strided_MADbench/oldtest.sh
@@ -6,22 +6,22 @@ export IOMODE=SYNC
 export FILETYPE=UNIQUE
 export IOMETHOD=POSIX
 #RECORD=16777216 # bytes read at once
-RECORD=4096 # bytes read at once
+RECORD=4194304 # 4Mbytes read at once
 #RECORD=1048576 # bytes read at once
 STRIDE=7 # set stride to $STRIDE * RECORD_SIZE
 NPROC=16 ##Num MPI procs
 FLUSH=1 ##flush writes
 
-#AMPLXE=/opt/intel/vtune_amplifier_2019/bin64/amplxe-cl
-#CONFIG_AMPLXE="-trace-mpi -collect hotspots -k enable-stack-collection=true -k stack-size=0 -k sampling-mode=hw"
-#CONFIG_AMPLXE="-trace-mpi -collect io -k kernel-stack=false -k spdk=true"
-#CONFIG_AMPLXE="-trace-mpi -collect memory-consumption"
-#CONFIG_AMPLXE="-trace-mpi -collect hpc-performance -k enable-stack-collection=true -k stack-size=2147483640 -k collect-affinity=true "
-#CONFIG_AMPLXE="-trace-mpi -collect platform-profiler"
-
-export LD_PRELOAD="/usr/lib/libcrosslayer.so"
-#export LD_PRELOAD="/usr/lib/libnopred.so"
 export FUTUREPREFETCH=10
 export TIMESPREFETCH=2
+
+DEV=/dev/sda4
+RASIZE=8192 #Pages allowed to readahead (320 = 1280 KB, 512 pages = 2048 KB)
+
+#export LD_PRELOAD="/usr/lib/libcrosslayer.so"
+export LD_PRELOAD="/usr/lib/libnopred.so"
+
+sudo blockdev --setra $RASIZE $DEV
+echo -n "blockdev getra = "; sudo blockdev --getra $DEV
 
 /usr/bin/time -v mpiexec.mpich -n $NPROC ./MADbench2_io 16384 30 1 8 64 1 1 $RECORD $STRIDE $FLUSH
