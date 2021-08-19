61ecc9647bab7559ca8685397b74411e315d85b0
shaleen <shaleen@node-0.linux-stable.lsm-pg0.wisc.cloudlab.us>
2021-08-19 05:17:21 -0500

    test ra size

--- appbench/apps/ior/test_setra.sh
+++ appbench/apps/ior/test_setra.sh
@@ -9,7 +9,6 @@
 APP="IOR"
 APPDIR=$PWD
 PREDICT=0
-#RESULTS_FILE=$OUTPUTDIR/$APP/13Aug-ior-sensitest.txt
 
 KB=1024
 MB=`echo "1024*$KB" | bc`
@@ -17,20 +16,22 @@ GB=`echo "1024*$MB" | bc`
 NPROC=1
 FILENAME="/mnt/ext4ramdisk/ior_test.dat"
 OUTFOLDER="$PWD/ramdisk-analysis"
-TRANSFERSZ=`echo "4*$KB" | bc`
+mkdir $OUTFOLDER
 NR_READS=200 ##Number of TRANSFERSZ reads by each mpi proc per segment
+TRANSFERSZ=`echo "4*$KB" | bc` #4K
 BLOCKSIZE=`echo "$NR_READS*$TRANSFERSZ" | bc`
 TOT_FILE_SIZE=`echo "30*$GB" | bc` #30GB
 NR_SEGMENTS=0
-echo "TotalFIle size" $TOT_FILE_SIZE
+echo "TotalFile size" $TOT_FILE_SIZE
 echo "BLOCKSIZE = " $BLOCKSIZE
-DEV="/dev/sda4"
+DEV="/dev/loop0"
 
-#declare -a setra=("256" "320" "512" "1024" "2048" "4096")
-declare -a setra=("256" "320" "512" "1024" "2048" "4096")
-declare -a nproc=("1" "2" "4" "8" "16" "32")
-#declare -a totsize=("30" "100" "200" "300") #in GB
-declare -a totsize=("30" "60" "80") #in GB
+declare -a setra=("256" "512" "1024" "2048" "4096" "8192" "16384")
+declare -a nproc=("16" "32")
+#declare -a transfersz=("4096" "32768" "131072" "1048576") #4K, 32K, 128K, 1M
+#declare -a transfersz=("4" "32" "128" "1024") #4K, 32K, 128K, 1M
+declare -a transfersz=("4") #4K, 32K, 128K, 1M
+declare -a totsize=("30") #in GB
 
 #declare -a transfersizearr=("4096") #transfer size
 #declare -a blockprodarr=("1024") #blocksize = transfersize*blockprod
@@ -74,25 +75,28 @@ WRITE=" -w "
 READ=" -r "
 
 
-for SETRA in "${setra[@]}"
+for NPROC in "${nproc[@]}"
 do
-    sudo blockdev --setra $SETRA $DEV
-    for TOTSIZE in "${totsize[@]}"
+    for TXSZ in "${transfersz[@]}"
     do
-        TOT_FILE_SIZE=`echo "$TOTSIZE*$GB" | bc`
-        RESULTS_FILE=${OUTFOLDER}/15Aug-ior-sensitest-${TOTSIZE}_GB_diff-file_setra-${SETRA}.txt
-        for NPROC in "${nproc[@]}"
+        TRANSFERSZ=`echo "$TXSZ*$KB" | bc`
+        BLOCKSIZE=`echo "$NR_READS*$TRANSFERSZ" | bc`
+        NR_SEGMENTS=`echo "$TOT_FILE_SIZE/($BLOCKSIZE*$NPROC)" | bc`
+        RESULTS_FILE=${OUTFOLDER}/19Aug-ior-sensitest-${TXSZ}K_reads-diff-file-${NPROC}-proc.txt
+        #RESULTS_FILE=${OUTFOLDER}/19Aug-ior-sensitest-${TXSZ}K_reads-diff-file-${NPROC}-proc.txt
+        for SETRA in "${setra[@]}"
         do
+            sudo blockdev --setra $SETRA $DEV
             rm $FILENAME*
-            NR_SEGMENTS=`echo "$TOT_FILE_SIZE/($BLOCKSIZE*$NPROC)" | bc`
             PARAMS="-e -o=$FILENAME -b=$BLOCKSIZE -t=$TRANSFERSZ -s=$NR_SEGMENTS $FILEPERPROC $KEEPFILE"
-            echo "NR_SEGMENTS=" $NR_SEGMENTS
 
+            #####################################################
             mpirun -np $NPROC ior $WRITE $PARAMS
-
             FlushDisk
+            #####################################################
 
             echo "############################################################" >> $RESULTS_FILE
+            echo -n "RA_SIZE= " >> $RESULTS_FILE
             sudo blockdev --getra $DEV >> $RESULTS_FILE
             $APPPREFIX mpirun -np $NPROC ior $READ $PARAMS $VERBOSE &>> $RESULTS_FILE
         done
