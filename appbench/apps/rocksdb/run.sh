2d191b203e705bd7da363c62c37d46f463209a1e
shaleen <shaleen@node-0.vanilla-linux5.lsm-pg0.wisc.cloudlab.us>
2021-09-15 15:05:01 -0500

    run added CLEAR_PWD

--- appbench/apps/rocksdb/run.sh
+++ appbench/apps/rocksdb/run.sh
@@ -9,7 +9,7 @@ WRITE_BUFF_SIZE=67108864
 NUM=1000000
 DBDIR=$DBHOME/DATA
 
-WORKLOADS="readrandom"
+WORKLOADS="readseq"
 WRITEARGS="--benchmarks=fillrandom --use_existing_db=0 --threads=1"
 READARGS="--benchmarks=$WORKLOADS --use_existing_db=1 --mmap_read=0"
 APPPREFIX="/usr/bin/time -v"
@@ -18,18 +18,20 @@ PARAMS="--db=$DBDIR --value_size=4096 --wal_dir=$DBDIR/WAL_LOG --sync=$SYNC --ke
 
 FlushDisk()
 {
-        sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
-        sudo sh -c "sync"
-        sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
-        sudo sh -c "sync"
+	sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+	sudo sh -c "sync"
+	sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+	sudo sh -c "sync"
 }
 
 SETPRELOAD()
 {
 	if [[ "$PREDICT" == "1" ]]; then
-	    export LD_PRELOAD=/usr/lib/libcrosslayer.so
+		echo "setting pred"
+		export LD_PRELOAD=/usr/lib/libcrosslayer.so
 	else
-	    export LD_PRELOAD=/usr/lib/libnopred.so
+		echo "setting nopred"
+		export LD_PRELOAD=/usr/lib/libnopred.so
 	fi
 }
 
@@ -40,34 +42,33 @@ BUILD_LIB()
 	cd $DBHOME
 }
 
+CLEAR_PWD()
+{
+	cd $DBDIR
+	rm -rf *.sst CURRENT IDENTITY LOCK MANIFEST-* OPTIONS-* WAL_LOG/
+	cd ..
+}
 
-cd $DBDIR
-rm -rf *.sst CURRENT IDENTITY LOCK MANIFEST-* OPTIONS-* WAL_LOG/
-cd ..
-
-
-#Build the predictor library
-BUILD_LIB
 
 #Run write workload twice
+CLEAR_PWD
 $DBHOME/db_bench $PARAMS $WRITEARGS &> out.txt
 
-echo "RUNNING CROSSLAYER.................."
-#$DBHOME/db_bench $PARAMS $WRITEARGS &> out.txt
+echo "RUNNING Vanilla.................."
 FlushDisk
-FlushDisk
+PREDICT=0
 SETPRELOAD
 $DBHOME/db_bench $PARAMS $READARGS
 FlushDisk
+export LD_PRELOAD=""
 
-
+CLEAR_PWD
 $DBHOME/db_bench $PARAMS $WRITEARGS &> out.txt
 
+FlushDisk
+echo "RUNNING Crosslayer.................."
+PREDICT=1
+SETPRELOAD
+strace $DBHOME/db_bench $PARAMS $READARGS
 export LD_PRELOAD=""
 FlushDisk
-echo "RUNNING VANILLA.................."
-$DBHOME/db_bench $PARAMS $READARGS
-FlushDisk
-
-
-
