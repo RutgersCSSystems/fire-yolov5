d58cabed5171980f34711958a7a2a3703275e125
shaleen <shaleen@node-0.vanilla-linux5.lsm-pg0.wisc.cloudlab.us>
2021-09-16 15:14:02 -0500

    qemu script changes

--- scripts/qemu/run_qemu.sh
+++ scripts/qemu/run_qemu.sh
@@ -11,50 +11,15 @@ KB=1024
 MB=`echo "1024*$KB" | bc`
 GB=`echo "1024*$MB" | bc`
 
-TOT_MEM=`echo "100*$GB" | bc`
-HALF_MEM=`echo "$TOT_MEM/2" | bc`
-
-#sudo qemu-system-x86_64 -kernel $KERNEL/vmlinuz-$VER -hda $QEMU_IMG_FILE -append "root=/dev/sda rw" --enable-kvm -m $QEMUMEM -smp maxcpus=16  -numa node,nodeid=0,cpus=0-4 -curses -vga std -numa node,nodeid=1,cpus=10-13
-
-#sudo qemu-system-x86_64 \
-#	-kernel $KERNEL/vmlinuz-$VER \
-#	-hda $QEMU_IMG_FILE \
-#	-append "root=/dev/sda rw" \
-#	--enable-kvm -m 100G \
-#	-numa node,nodeid=0,cpus=0-39,mem=100G \
-#	-smp sockets=1,cores=10,threads=2,maxcpus=40 \
-#	-curses -device e1000,netdev=net0 \
-#	-netdev user,id=net0,hostfwd=tcp::5555-:22
+HALF_MEM=`echo "$QEMUMEM/2" | bc`
 
 sudo qemu-system-x86_64 \
 	-kernel $KERNEL/vmlinuz-$VER \
 	-hda $QEMU_IMG_FILE \
 	-append "root=/dev/sda rw" \
-	--enable-kvm -m $TOT_MEM \
-	-numa node,nodeid=0,cpus=0-9,mem=$HALF_MEM \
-	-numa node,nodeid=1,cpus=20-39,mem=$HALF_MEM \
+	--enable-kvm -m ${QEMUMEM}G \
+	-numa node,nodeid=0,cpus=0-9,mem=${HALF_MEM}G \
+	-numa node,nodeid=1,cpus=20-39,mem=${HALF_MEM}G \
 	-smp sockets=2,cores=5,threads=2,maxcpus=40 \
 	-curses -device e1000,netdev=net0 \
 	-netdev user,id=net0,hostfwd=tcp::5555-:22
-
-
-#sudo qemu-system-x86_64 \
-#    -kernel $KERNEL/vmlinuz-$VER \
-#    -hda $QEMU_IMG_FILE \
-#    --enable-kvm -m $QEMUMEM \
-#    -numa node,nodeid=0,cpus=0-7,mem=20G \
-#    -numa node,nodeid=1,cpus=16-23,mem=20G \
-#    -smp sockets=2,cores=4,threads=2,maxcpus=32 \
-#    -device e1000,netdev=net0 \
-#    -netdev user,id=net0,hostfwd=tcp::5555-:22 \
-#    --nographic -serial mon:stdio -append "nokaslr console=ttyS0 root=/dev/sda rw net.ifnames=0 biosdevname=0 earlyprint=serial loglevel=8 ro rootfstype=ext4 mitigations=off"
-
-#-device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22 -redir tcp:4444::4444 -redir tcp:3333::3333 #-net user,hostfwd=tcp:127.0.0.1:2222-:22   #-netdev tap,id=mynet0,ifname=eth0 -device e1000,netdev=mynet0
-
-#-nographic #-display curses
-#sudo qemu-system-x86_64 -nographic -kernel $KERNEL/vmlinuz-4.17.0 -hda qemu-image.img -append "root=/dev/sda rw console=ttyAMA0 console=ttyS0" --enable-kvm -m 16G -numa node,nodeid=0,cpus=0-4 -numa node,nodeid=1,cpus=10-13
-
-#sudo qemu-system-x86_64 -kernel $KERNEL/vmlinuz-$VER -hda $QEMU_IMG_FILE -append "root=/dev/sda rw" --enable-kvm -m $QEMUMEM -numa node,nodeid=0,cpus=0-4 -numa node,nodeid=1,cpus=10-13 -nographic -net nic,macaddr=56:44:45:30:31:32,vlan=0 -net tap,script=no,ifname=tap0,vlan=0 &
-
-#--curses
-
