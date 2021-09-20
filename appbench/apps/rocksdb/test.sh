d08e0ffd004a7f56ee3bc1b2821448edbcf85234
shaleen <shaleen@node-0.vanilla-linux5.lsm-pg0.wisc.cloudlab.us>
2021-09-19 21:21:47 -0500

    testing nopred vs onlyapppred

--- appbench/apps/rocksdb/test.sh
+++ appbench/apps/rocksdb/test.sh
@@ -5,15 +5,15 @@ VALUE_SIZE=4096
 SYNC=0
 KEYSIZE=1000
 WRITE_BUFF_SIZE=67108864
-NUM=1000000
+NUM=100000
 DBDIR=$DBHOME/DATA
 
-WORKLOADS="readseq"
+WORKLOAD="readseq"
 WRITEARGS="--benchmarks=fillrandom --use_existing_db=0 --threads=1"
-READARGS="--benchmarks=$WORKLOADS --use_existing_db=1 --mmap_read=0"
+READARGS="--benchmarks=$WORKLOAD --use_existing_db=1 --mmap_read=0 --threads=$THREAD"
 APPPREFIX="/usr/bin/time -v"
 
-PARAMS="--db=$DBDIR --value_size=4096 --wal_dir=$DBDIR/WAL_LOG --sync=$SYNC --key_size=100 --write_buffer_size=67108864 --threads=$THREAD --num=$NUM"
+PARAMS="--db=$DBDIR --value_size=$VALUE_SIZE --wal_dir=$DBDIR/WAL_LOG --sync=$SYNC --key_size=$KEYSIZE --write_buffer_size=$WRITE_BUFF_SIZE --num=$NUM"
 
 FlushDisk()
 {
@@ -28,9 +28,12 @@ SETPRELOAD()
 	if [[ "$1" == "1" ]]; then
 		echo "setting pred"
 		export LD_PRELOAD=/usr/lib/libcrosslayer.so
-	else
+     elif [[ "$1" == "0" ]]; then
 		echo "setting nopred"
 		export LD_PRELOAD=/usr/lib/libnopred.so
+      else
+          echo "only app pred"
+		export LD_PRELOAD=/usr/lib/libonlyapppred.so
 	fi
 }
 
@@ -51,11 +54,26 @@ CLEAR_PWD()
 
 #Run write workload twice
 CLEAR_PWD
-$DBHOME/db_bench $PARAMS $WRITEARGS
+$DBHOME/db_bench $PARAMS $WRITEARGS > /dev/null
 
-echo "RUNNING Vanilla.................."
+echo "RUNNING Only App Pred.................."
+FlushDisk
+SETPRELOAD -1
+$DBHOME/db_bench $PARAMS $READARGS
+#/users/shaleen/ssd/ltrace/ltrace -w 5 -rfSC -l /usr/lib/libnopred.so $DBHOME/db_bench $PARAMS $READARGS
+export LD_PRELOAD=""
+FlushDisk
+
+
+
+#Run write workload twice
+CLEAR_PWD
+$DBHOME/db_bench $PARAMS $WRITEARGS > /dev/null
+
+echo "RUNNING No Pred.................."
 FlushDisk
 SETPRELOAD 0
-strace $DBHOME/db_bench $PARAMS $READARGS
-FlushDisk
+$DBHOME/db_bench $PARAMS $READARGS
+#/users/shaleen/ssd/ltrace/ltrace -w 5 -rfSC -l /usr/lib/libnopred.so $DBHOME/db_bench $PARAMS $READARGS
 export LD_PRELOAD=""
+FlushDisk
