4547d80d12d231f24e481b798202cef08d96bfba
shaleen <shaleen@node-0.newnvme.lsm-pg0.utah.cloudlab.us>
2021-09-26 19:54:24 -0600

    biolatency

--- scripts/compile-install/install_biolatency.sh
+++ scripts/compile-install/install_biolatency.sh
@@ -4,10 +4,12 @@
 
 sudo apt update
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
-echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/iovisor.list
-sudo apt update
-sudo apt-get install bcc-tools libbcc-examples blktrace collectl perf-tools-unstable nmon htop -y
+sudo apt-get -y install bison build-essential cmake flex git libedit-dev \
+  libllvm6.0 llvm-6.0-dev libclang-6.0-dev python zlib1g-dev libelf-dev libfl-dev python3-distutils
 
-echo 'export PATH=/usr/share/bcc/tools:$PATH' >> ~/.bashrc
-echo "source ~/.bashrc #do it before using biolatency"
+git clone https://github.com/iovisor/bcc.git
+mkdir bcc/build; cd bcc/build
+cmake ..
+make
+sudo make install
+
