ba19ded2d250ff82b5c55ddc1d7ec161717e1632
sudarsun <sudarsun>
2022-02-13 03:35:18 +0000

    clean the scripts

--- appbench/apps/strided_MADbench/test.sh
+++ appbench/apps/strided_MADbench/test.sh
@@ -15,18 +15,10 @@ STRIDE=7 # set stride to $STRIDE * RECORD_SIZE
 NPROC=1 ##Num MPI procs
 FLUSH=1 ##flush writes
 DEV=/dev/sda4
-#force_page_cache_readahead - max_pages = 320, 512, 1024, 2048 (rasize = $maxpages*4096/512)
 declare -a rasize=("2560" "4096" "8192" "16384") 
 RASIZE=8192 #Pages allowed to readahead (320 = 1280 KB, 512 pages = 2048 KB)
 
-#AMPLXE=/opt/intel/vtune_amplifier_2019/bin64/amplxe-cl
-#CONFIG_AMPLXE="-trace-mpi -collect hotspots -k enable-stack-collection=true -k stack-size=0 -k sampling-mode=hw"
-#CONFIG_AMPLXE="-trace-mpi -collect io -k kernel-stack=false -k spdk=true"
-#CONFIG_AMPLXE="-trace-mpi -collect memory-consumption"
-#CONFIG_AMPLXE="-trace-mpi -collect hpc-performance -k enable-stack-collection=true -k stack-size=2147483640 -k collect-affinity=true "
-#CONFIG_AMPLXE="-trace-mpi -collect platform-profiler"
 
-#export LD_PRELOAD="/usr/local/lib/libdarshan.so"
 export LD_PRELOAD="/usr/lib/libcrosslayer.so"
 #export LD_PRELOAD="/usr/lib/libnopred.so"
 export FUTUREPREFETCH=10
@@ -34,9 +26,5 @@ export TIMESPREFETCH=15
 
 sudo blockdev --setra $RASIZE $DEV
 echo -n "blockdev getra = "; sudo blockdev --getra $DEV
-#/usr/bin/time -v mpiexec.mpich -n $NPROC ./MADbench2_io 16384 30 1 8 64 1 1 $RECORD $STRIDE $FLUSH
 /usr/bin/time -v mpiexec.mpich -n $NPROC ./MADbench2_io 16384 5 1 8 64 1 1 $RECORD $STRIDE $FLUSH
-#/usr/bin/time -v mpiexec.mpich -n $NPROC ./MADbench2_io 8192 5 1 8 64 1 1 $RECORD $STRIDE $FLUSH
-#TIMESPREFETCH=2 /usr/bin/time -v mpiexec -n $NPROC ./MADbench2_io 16384 3 1 8 64 1 1 $RECORD $STRIDE $FLUSH
-
 export LD_PRELOAD=
