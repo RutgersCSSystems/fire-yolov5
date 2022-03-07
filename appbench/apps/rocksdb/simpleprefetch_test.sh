aff0bcf8348ded12b5dd9fda58151ad61a9af902
shaleen <shaleen@node-0.small-nvme.lsm-pg0.utah.cloudlab.us>
2022-03-06 18:25:43 -0700

    simpleprefetch testing rocksdb

---
+++ appbench/apps/rocksdb/simpleprefetch_test.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+DBHOME=$PWD
+THREAD=4
+VALUE_SIZE=4096
+SYNC=0
+KEYSIZE=1000
+WRITE_BUFF_SIZE=67108864
+NUM=1000000
+DBDIR=$DBHOME/DATA
+
+WORKLOAD="readseq"
+#WORKLOAD="readrandom"
+#WORKLOAD="readreverse"
+WRITEARGS="--benchmarks=fillrandom --use_existing_db=0 --threads=1"
+READARGS="--benchmarks=$WORKLOAD --use_existing_db=1 --mmap_read=0 --threads=$THREAD --advise_random_on_open=false"
+#READARGS="--benchmarks=$WORKLOAD --use_existing_db=1 --mmap_read=0 --threads=$THREAD --advise_random_on_open=false --readahead_size=2097152 --compaction_readahead_size=2097152 --log_readahead_size=2097152"
+APPPREFIX="/usr/bin/time -v"
+
+declare -a num_arr=("1000000" "20000000" "30000000")
+
+FlushDisk()
+{
+    sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+    sudo sh -c "sync"
+    sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+    sudo sh -c "sync"
+    sudo dmesg --clear
+    sleep 5
+}
+
+
+SETPRELOAD()
+{
+    if [[ "$1" == "APPLIBOS" ]]; then ##All three
+        echo "setting pred"
+        export LD_PRELOAD=/usr/lib/libcrosslayer.so
+    elif [[ "$1" == "NOPRED" ]]; then ##None
+        echo "setting nopred"
+        export LD_PRELOAD=/usr/lib/libnopred.so
+    elif [[ "$1" == "ONLYAPP" ]]; then
+        echo "only app pred"
+        export LD_PRELOAD=/usr/lib/libonlyapppred.so
+    elif [[ "$1" == "ONLYLIB" ]]; then
+        echo "only Lib pred"
+        export LD_PRELOAD=/usr/lib/libonlylibpred.so
+    elif [[ "$1" == "ONLYOS" ]]; then
+        echo "only OS pred"
+        export LD_PRELOAD=/usr/lib/libonlyospred.so
+    elif [[ "$1" == "APPOS" ]]; then
+        echo "App+OS pred"
+        export LD_PRELOAD=/usr/lib/libos_apppred.so
+    elif [[ "$1" == "LIBOS" ]]; then
+        echo "Lib+OS pred"
+        export LD_PRELOAD=/usr/lib/libos_libpred.so
+    elif [[ "$1" == "ONLYINTERCEPT" ]]; then
+        echo "Only Intercepting"
+        export LD_PRELOAD=/usr/lib/libonlyintercept.so
+    elif [[ "$1" == "CACHELIMIT" ]]; then
+        echo "OS and LIB pred with Cache Limit"
+        export LD_PRELOAD=/usr/lib/libcache_lim_os_libpred.so
+    elif [[ "$1" == "FETCHALL" ]]; then
+        echo "OS and LIB pred without Cache Limit"
+        export LD_PRELOAD=/usr/lib/libos_fetch_at_open.so
+    elif [[ "$1" == "FETCHALLSINGLE" ]]; then
+        echo "OS and LIB pred without Cache Limit"
+        export LD_PRELOAD=/usr/lib/libos_fetch_at_open_single.so
+    elif [[ "$1" == "SIMPLEBGPREFETCH" ]]; then
+        echo "Simple BG prefetcher"
+        export LD_PRELOAD=/usr/lib/libsimpleprefetcher.so
+    elif [[ "$1" == "SIMPLENOPREFETCH" ]]; then
+        echo "Simple NO prefetcher"
+        export LD_PRELOAD=/usr/lib/libsimplenoprefetcher.so
+    elif [[ "$1" == "SIMPLEBGFULLPREFETCH" ]]; then
+        echo "Simple BG FULL prefetcher"
+        export LD_PRELOAD=/usr/lib/libsmpl_fullprefetcher.so
+    fi
+
+    ##export TARGET_GPPID=$PPID
+}
+
+CLEAR_PWD()
+{
+    cd $DBDIR
+    rm -rf *.sst CURRENT IDENTITY LOCK MANIFEST-* OPTIONS-* WAL_LOG/
+    cd ..
+}
+
+
+CLEAN_AND_WRITE()
+{
+    echo "in ${FUNCNAME[0]}"
+
+    export LD_PRELOAD=""
+    CLEAR_PWD
+    $DBHOME/db_bench $PARAMS $WRITEARGS
+    FlushDisk
+
+    ##Condition the DB to get Stable results
+    $DBHOME/db_bench $1 $READARGS 
+    FlushDisk
+    $DBHOME/db_bench $1 $READARGS 
+    FlushDisk
+}
+
+
+for NUM in "${num_arr[@]}"
+do
+    PARAMS="--db=$DBDIR --value_size=$VALUE_SIZE --wal_dir=$DBDIR/WAL_LOG --sync=$SYNC --key_size=$KEYSIZE --write_buffer_size=$WRITE_BUFF_SIZE --num=$NUM"
+
+    CLEAN_AND_WRITE
+    FlushDisk
+
+    echo "\nRUNNING Vanilla................."
+    LD_PRELOAD=""
+    $DBHOME/db_bench $PARAMS $READARGS
+    FlushDisk
+
+    echo "\nRUNNING Disable FADV_RANDOM ................."
+    SETPRELOAD "SIMPLENOPREFETCH"
+    $DBHOME/db_bench $PARAMS $READARGS
+    LD_PRELOAD=""
+    FlushDisk
+
+    echo "\nRUNNING SIMPLE PREFETCHER ................."
+    SETPRELOAD "SIMPLEBGPREFETCH"
+    $DBHOME/db_bench $PARAMS $READARGS
+    LD_PRELOAD=""
+    FlushDisk
+
+    echo "\nRUNNING SIMPLE FULL PREFETCHER ................."
+    SETPRELOAD "SIMPLEBGFULLPREFETCH"
+    $DBHOME/db_bench $PARAMS $READARGS
+    LD_PRELOAD=""
+    FlushDisk
+
+    exit
+
+done
+
