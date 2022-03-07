2a5d9da84713b26ca23ccb441c03ca8582188786
shaleen <shaleen@node-0.small-nvme.lsm-pg0.utah.cloudlab.us>
2022-03-07 16:06:39 -0700

    script update

--- shared_libs/simple_prefetcher/compile.sh
+++ shared_libs/simple_prefetcher/compile.sh
@@ -2,7 +2,7 @@
 
 ##This compile script will compile the preload libraries and install them
 
-PREFETCH_SIZE=40 ##in nr of pages
+PREFETCH_SIZE=120 ##in nr of pages
 
 make NR_RA_PG=$PREFETCH_SIZE -j
 make install
