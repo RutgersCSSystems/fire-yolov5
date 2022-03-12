3e0b2608ee57a2db30f8ec75f250c148916fc32b
shaleen <shaleen@node-0.lg-orignix.lsm-pg0.utah.cloudlab.us>
2022-03-12 10:26:01 -0700

    tester

--- appbench/apps/rocksdb/simpleprefetch_test.sh
+++ appbench/apps/rocksdb/simpleprefetch_test.sh
@@ -16,7 +16,7 @@ READARGS="--benchmarks=$WORKLOAD --use_existing_db=1 --mmap_read=0 --threads=$TH
 #READARGS="--benchmarks=$WORKLOAD --use_existing_db=1 --mmap_read=0 --threads=$THREAD --advise_random_on_open=false --readahead_size=2097152 --compaction_readahead_size=2097152 --log_readahead_size=2097152"
 APPPREFIX="/usr/bin/time -v"
 
-declare -a num_arr=("1000000" "20000000" "30000000")
+declare -a num_arr=("1000000")
 
 FlushDisk()
 {
@@ -31,39 +31,15 @@ FlushDisk()
 
 SETPRELOAD()
 {
-    if [[ "$1" == "APPLIBOS" ]]; then ##All three
-        printf "setting pred\n"
-        export LD_PRELOAD=/usr/lib/libcrosslayer.so
-    elif [[ "$1" == "NOPRED" ]]; then ##None
-        printf "setting nopred\n"
-        export LD_PRELOAD=/usr/lib/libnopred.so
-    elif [[ "$1" == "ONLYAPP" ]]; then
-        printf "only app pred\n"
-        export LD_PRELOAD=/usr/lib/libonlyapppred.so
-    elif [[ "$1" == "ONLYLIB" ]]; then
-        printf "only Lib pred\n"
-        export LD_PRELOAD=/usr/lib/libonlylibpred.so
-    elif [[ "$1" == "ONLYOS" ]]; then
-        printf "only OS pred\n"
-        export LD_PRELOAD=/usr/lib/libonlyospred.so
-    elif [[ "$1" == "APPOS" ]]; then
-        printf "App+OS pred\n"
-        export LD_PRELOAD=/usr/lib/libos_apppred.so
-    elif [[ "$1" == "LIBOS" ]]; then
-        printf "Lib+OS pred\n"
-        export LD_PRELOAD=/usr/lib/libos_libpred.so
-    elif [[ "$1" == "ONLYINTERCEPT" ]]; then
-        printf "Only Intercepting\n"
-        export LD_PRELOAD=/usr/lib/libonlyintercept.so
-    elif [[ "$1" == "CACHELIMIT" ]]; then
-        printf "OS and LIB pred with Cache Limit\n"
-        export LD_PRELOAD=/usr/lib/libcache_lim_os_libpred.so
-    elif [[ "$1" == "FETCHALL" ]]; then
-        printf "OS and LIB pred without Cache Limit\n"
-        export LD_PRELOAD=/usr/lib/libos_fetch_at_open.so
-    elif [[ "$1" == "FETCHALLSINGLE" ]]; then
-        printf "OS and LIB pred without Cache Limit\n"
-        export LD_PRELOAD=/usr/lib/libos_fetch_at_open_single.so
+    if [[ "$1" == "VANILLA" ]]; then ##All three
+        printf "setting Vanilla\n"
+        export LD_PRELOAD=/usr/lib/lib_Vanilla.so
+    elif [[ "$1" == "OSONLY" ]]; then ##None
+        printf "setting OSonly\n"
+        export LD_PRELOAD=/usr/lib/lib_OSonly.so
+    elif [[ "$1" == "CFNMB" ]]; then
+        printf "only app CFNMB\n"
+        export LD_PRELOAD=/usr/lib/lib_CFNMB.so
     elif [[ "$1" == "SIMPLEBGPREFETCH" ]]; then
         printf "Simple BG prefetcher\n"
         export LD_PRELOAD=/usr/lib/libsimpleprefetcher.so
@@ -110,27 +86,28 @@ for NUM in "${num_arr[@]}"
 do
     PARAMS="--db=$DBDIR --value_size=$VALUE_SIZE --wal_dir=$DBDIR/WAL_LOG --sync=$SYNC --key_size=$KEYSIZE --write_buffer_size=$WRITE_BUFF_SIZE --num=$NUM"
 
-    CLEAN_AND_WRITE
+    #CLEAN_AND_WRITE
     FlushDisk
 
     #printf "\nRUNNING Vanilla.................\n"
-    #LD_PRELOAD=""
+    #SETPRELOAD "VANILLA"
     #$DBHOME/db_bench $PARAMS $READARGS
     #FlushDisk
 
-    printf "\nRUNNING Disable FADV_RANDOM .................\n"
-    SETPRELOAD "SIMPLENOPREFETCH"
+
+    #printf "\nRUNNING OSONLY...............\n"
+    #SETPRELOAD "OSONLY"
+    #$DBHOME/db_bench $PARAMS $READARGS
+    #LD_PRELOAD=""
+    #FlushDisk
+
+    printf "\nRUNNING CROSS_FILERA_NOPRED_MAXMEM_BG................\n"
+    SETPRELOAD "CFNMB"
     $DBHOME/db_bench $PARAMS $READARGS
     LD_PRELOAD=""
     FlushDisk
 
-    exit
-
-    printf "\nRUNNING SIMPLE PREFETCHER .................\n"
-    SETPRELOAD "SIMPLEBGPREFETCH"
-    $DBHOME/db_bench $PARAMS $READARGS
-    LD_PRELOAD=""
-    FlushDisk
+    exit 
 
     printf "\nRUNNING PREAD_RA .................\n"
     SETPRELOAD "SIMPLEPREADRA"
