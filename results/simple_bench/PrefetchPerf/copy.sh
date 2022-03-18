d848c09597d636233918b29dc54879ea1c5e2925
Shaleen Garg <shaleengarg.in@gmail.com>
2022-03-17 21:59:31 -0400

    copy resutls to plots

---
+++ results/simple_bench/PrefetchPerf/copy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#This scripts only copies files from results folder to plots folder
+#No other work done here
+
+filesize=40 ##GB
+read_size=20 ## in pages
+
+declare -a workload_arr=("read_seq" "read_rand") ##read binaries
+declare -a experiment=("VANILLA" "CFNMB" "CFPMB" "CBPMB")
+
+
+OUTFILENAME="filesz-${filesize}_Readsz-${read_size}"
+for WORKLOAD in "${workload_arr[@]}"
+do
+        for EXPERIMENT in "${experiment[@]}"
+        do
+                cp $PWD/$EXPERIMENT/$WORKLOAD/$OUTFILENAME $PWD/plots/$WORKLOAD/${EXPERIMENT}_${WORKLOAD}.plot
+        done
+done
