1654822ac8f9c4388e430cd3de619693045b7f77
shaleen <shaleen@node-0.large-mod2.lsm-pg0.utah.cloudlab.us>
2022-03-20 17:17:28 -0600

    ext4ramdisk

--- scripts/run/generic_funcs.sh
+++ scripts/run/generic_funcs.sh
@@ -14,70 +14,70 @@ APPPREFIX="/usr/bin/time -v"
 
 FlushDisk()
 {
-    sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
-    sudo sh -c "sync"
-    sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
-    sudo sh -c "sync"
-    sleep 5
+        sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+        sudo sh -c "sync"
+        sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+        sudo sh -c "sync"
+        sleep 5
 }
 
 SLEEPNOW() {
-    sleep 2
+        sleep 2
 }
 
 ENABLE_LOCK_STATS()
 {
-	sudo sh -c "echo 0 > /proc/lock_stat"
-	#sudo sh -c "echo 1 > /proc/sys/kernel/lock_stat"
+        sudo sh -c "echo 0 > /proc/lock_stat"
+        #sudo sh -c "echo 1 > /proc/sys/kernel/lock_stat"
 }
 
 DISABLE_LOCK_STATS()
 {
-	sudo sh -c "echo 0 > /proc/sys/kernel/lock_stat"
+        sudo sh -c "echo 0 > /proc/sys/kernel/lock_stat"
 }
 
 
 ##Builds the preload library
 BUILD_PRED_LIB()
 {
-    pushd $PREDICT_LIB_DIR
-    ./compile.sh
-    popd
+        pushd $PREDICT_LIB_DIR
+        ./compile.sh
+        popd
 }
 
 
 SETPRELOAD()
 {
-    if [[ "$1" == "VANILLA" ]]; then ##No library involvement
-        printf "Running Vanilla\n"
-        export LD_PRELOAD=""
-    elif [[ "$1" == "OSONLY" ]]; then ##Only OS prefetching enabled
-        printf "Only OS prefetching enabled\n"
-        export LD_PRELOAD=/usr/lib/lib_OSonly.so
-    elif [[ "$1" == "CBNMB" ]]; then
-        printf "Cross_BlockRA_NoPred_MaxMem_BG\n"
-        export LD_PRELOAD=/usr/lib/lib_CBNMB.so
-    elif [[ "$1" == "CFNMB" ]]; then
-        printf "Cross_FileRA_NoPred_MaxMem_BG\n"
-        export LD_PRELOAD=/usr/lib/lib_CFNMB.so
-    elif [[ "$1" == "CFPMB" ]]; then
-        printf "Cross_FileRA_Pred_MaxMem_BG\n"
-        export LD_PRELOAD=/usr/lib/lib_CFPMB.so
-    elif [[ "$1" == "CBPMB" ]]; then
-        printf "Cross_BlockRA_Pred_MaxMem_BG\n"
-        export LD_PRELOAD=/usr/lib/lib_CBPMB.so
-    fi
-    ##export TARGET_GPPID=$PPID
+        if [[ "$1" == "VANILLA" ]]; then ##No library involvement
+                printf "Running Vanilla\n"
+                export LD_PRELOAD=""
+        elif [[ "$1" == "OSONLY" ]]; then ##Only OS prefetching enabled
+                printf "Only OS prefetching enabled\n"
+                export LD_PRELOAD=/usr/lib/lib_OSonly.so
+        elif [[ "$1" == "CBNMB" ]]; then
+                printf "Cross_BlockRA_NoPred_MaxMem_BG\n"
+                export LD_PRELOAD=/usr/lib/lib_CBNMB.so
+        elif [[ "$1" == "CFNMB" ]]; then
+                printf "Cross_FileRA_NoPred_MaxMem_BG\n"
+                export LD_PRELOAD=/usr/lib/lib_CFNMB.so
+        elif [[ "$1" == "CFPMB" ]]; then
+                printf "Cross_FileRA_Pred_MaxMem_BG\n"
+                export LD_PRELOAD=/usr/lib/lib_CFPMB.so
+        elif [[ "$1" == "CBPMB" ]]; then
+                printf "Cross_BlockRA_Pred_MaxMem_BG\n"
+                export LD_PRELOAD=/usr/lib/lib_CBPMB.so
+        fi
+        ##export TARGET_GPPID=$PPID
 }
 
 UNSETPRELOAD(){
-    export LD_PRELOAD=""
+        export LD_PRELOAD=""
 }
 
 #clears cache and unloads
 REFRESH() {
-    UNSETPRELOAD
-    FlushDisk
+        UNSETPRELOAD
+        FlushDisk
 }
 
 #Checks if the folder exits, if not, create new
@@ -92,41 +92,91 @@ CREATE_OUTFOLDER() {
 
 #returns the min out of list of numbers
 min_number() {
-	printf "%s\n" "$@" | sort -g | head -n1
+        printf "%s\n" "$@" | sort -g | head -n1
 }
 
 #returns the max out of list of numbers
 max_number() {
-	printf "%s\n" "$@" | sort -gr | head -n1
+        printf "%s\n" "$@" | sort -gr | head -n1
 }
 
 
+umount_ext4ramdisk() {
+        sudo umount /mnt/ext4ramdisk
+        sudo rm -rf /mnt/ramdisk/ext4.image
+        sudo umount /mnt/ramdisk
+}
+
+mount_ext4ramdisk() {
+        DISKSZ=$1 #Input in MB
+
+        umount_ext4ramdisk
+
+        sudo mkdir /mnt/ramdisk
+        sudo mount -t ramfs ramfs /mnt/ramdisk
+        sudo dd if=/dev/zero of=/mnt/ramdisk/ext4.image bs=1M count=$DISKSZ
+        sudo mkfs.ext4 -F /mnt/ramdisk/ext4.image
+        sudo mkdir /mnt/ext4ramdisk
+        sudo mount -o loop /mnt/ramdisk/ext4.image /mnt/ext4ramdisk
+        sudo chown -R $USER /mnt/ext4ramdisk
+        #sudo ln -s /mnt/ext4ramdisk $APPBENCH/shared_data
+}
+
 ##Reduces size of ram if needed
-SETUPEXTRAM() {
+##1 numa node
+SETUPEXTRAM_1() {
 
-    let CAPACITY=$1
+        let APP_BUDGET=$1 ##in MB
 
-    let SPLIT=$CAPACITY/2
-    echo "SPLIT" $SPLIT
+        echo "APP Budget = $APP_BUDGET MB"
 
-    sudo rm -rf  /mnt/ext4ramdisk0/*
-    sudo rm -rf  /mnt/ext4ramdisk1/*
+        let SPLIT=$APP_BUDGET ##in MB
 
-    $NVMBASE/scripts/mount/umount_ext4ramdisk.sh 0
-    $NVMBASE/scripts/mount/umount_ext4ramdisk.sh 1
+        sudo rm -rf  /mnt/ext4ramdisk/*
 
-    SLEEPNOW
+        umount_ext4ramdisk
 
-    NUMAFREE0=`numactl --hardware | grep "node 0 free:" | awk '{print $4}'`
-    NUMAFREE1=`numactl --hardware | grep "node 1 free:" | awk '{print $4}'`
+        SLEEPNOW
 
-    let DISKSZ=$NUMAFREE0-$SPLIT-712
-    let ALLOCSZ=$NUMAFREE1-$SPLIT-712
+        NUMAFREE0=`numactl --hardware | grep "node 0 free:" | awk '{print $4}'`
 
-    echo "NODE 0 $DISKSZ NODE 1 $ALLOCSZ"
+        let DISKSZ=$NUMAFREE0-$SPLIT-500
 
-    $NVMBASE/scripts/mount/mount_ext4ramdisk.sh $DISKSZ 0
-    $NVMBASE/scripts/mount/mount_ext4ramdisk.sh $ALLOCSZ 1
+        echo "creating ramdisk of size $DISKSZ MB"
 
-    SLEEPNOW
+        mount_ext4ramdisk $DISKSZ
+
+        SLEEPNOW
+}
+
+##Reduces size of ram if needed
+##NOT WORKING RIGHT NOW
+##2 numa nodes
+SETUPEXTRAM_2() {
+
+        let CAPACITY=$1
+
+        let SPLIT=$CAPACITY/2
+        echo "SPLIT" $SPLIT
+
+        sudo rm -rf  /mnt/ext4ramdisk0/*
+        sudo rm -rf  /mnt/ext4ramdisk1/*
+
+        $NVMBASE/scripts/mount/umount_ext4ramdisk.sh 0
+        $NVMBASE/scripts/mount/umount_ext4ramdisk.sh 1
+
+        SLEEPNOW
+
+        NUMAFREE0=`numactl --hardware | grep "node 0 free:" | awk '{print $4}'`
+        NUMAFREE1=`numactl --hardware | grep "node 1 free:" | awk '{print $4}'`
+
+        let DISKSZ=$NUMAFREE0-$SPLIT-712
+        let ALLOCSZ=$NUMAFREE1-$SPLIT-712
+
+        echo "NODE 0 $DISKSZ NODE 1 $ALLOCSZ"
+
+        $NVMBASE/scripts/mount/mount_ext4ramdisk.sh $DISKSZ 0
+        $NVMBASE/scripts/mount/mount_ext4ramdisk.sh $ALLOCSZ 1
+
+        SLEEPNOW
 }
