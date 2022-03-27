bad976b3ad27a5d3c45495e05ea517ed2864c5f5
Shaleen Garg <shaleengarg.in@gmail.com>
2022-03-27 17:35:45 -0400

    Dont ifdef user interfacing structs

--- linux-5.14.0/include/linux/crosslayer.h
+++ linux-5.14.0/include/linux/crosslayer.h
@@ -70,14 +70,14 @@ struct read_ra_req {
 //
     unsigned long nr_free; //nr pages that are free in memory
     
-#ifdef CONFIG_CROSS_FILE_BITMAP
+//#ifdef CONFIG_CROSS_FILE_BITMAP
     /*
      * The following are populated by the kernel
      * and returned to user space
      */
     void *data;  //page bitmap for readahead file
     unsigned long nr_relevant_bits; //number of bits relevant for the file
-#endif
+//#endif
 };
 
 
