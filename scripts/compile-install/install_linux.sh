617279dffa1fb3282411d6068795ea25a6555841
shaleen <shaleen@node-0.large-modified.lsm-pg0.utah.cloudlab.us>
2022-04-01 23:59:12 -0600

    install typical linux kernel

--- scripts/compile-install/install_linux.sh
+++ scripts/compile-install/install_linux.sh
@@ -6,39 +6,25 @@ if [ -z "$NVMBASE" ]; then
     exit 1
 fi
 
-VERSION=4.19.197
+#wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.14.tar.gz
+
+VERSION=5.14
 KERNEL=linux-$VERSION
 PROC=`nproc`
 export CONCURRENCY_LEVEL=$PROC
 export CONCURRENCYLEVEL=$PROC 
 
-cd $NVMBASE
-#git clone https://github.com/shaleengarg/$KERNEL.git
-wget https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/$KERNEL.tar.gz
+cd $NVMBASE/../
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$VERSION.tar.gz
 tar -xf  $KERNEL.tar.gz
 cp $NVMBASE/scripts/helperscripts/linuxMakefile-$VERSION $KERNEL/Makefile
+cp $NVMBASE/linux-5.14.0/modifiednix.config $KERNEL/.config
 if [ $? -ne 0 ]
 then
 	echo "no custom makefile for $KERNEL"
 	exit
 fi
 cp $NVMBASE/scripts/helperscripts/compile_deb.sh $KERNEL/compile_deb.sh
-cp $NVMBASE/scripts/helperscripts/compile_make.sh $KERNEL/compile_make.sh
 cd $KERNEL
 
-sudo apt-get update
-sudo apt-get install -y libdpkg-dev kernel-package libncurses5-dev build-essential bison flex libssl-dev libelf-dev
-
-
 ./compile_deb.sh ##To be called only once, next time, call compile_make.sh
-
-exit
-
-#touch REPORTING-BUGS
-#make distclean -j
-#make menuconfig
-#sudo fakeroot make-kpkg -j$PROCS --initrd kernel-image kernel-headers
-
-##dpkg only if this is the first time, next time, only need to compile
-##TODO
-#sudo dpkg -i ../*image*.deb ../*header*.deb
