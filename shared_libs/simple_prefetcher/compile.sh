bb27141cb0b973ed275a83fe69f13cd38a463484
shaleen <shaleen@node-0.shaleen-121907.lsm-pg0.utah.cloudlab.us>
2022-04-07 09:57:10 -0600

    insights

--- shared_libs/simple_prefetcher/compile.sh
+++ shared_libs/simple_prefetcher/compile.sh
@@ -2,7 +2,8 @@
 
 ##This compile script will compile the preload libraries and install them
 
-PREFETCH_SIZE=120 ##in nr of pages
+#OS doesnt prefetch more than 256 pages in vanilla kernel
+PREFETCH_SIZE=512 ##in nr of pages
 NR_WORKERS=4
 
 make NR_RA_PAGES=$PREFETCH_SIZE NR_WORKERS=$NR_WORKERS -j
