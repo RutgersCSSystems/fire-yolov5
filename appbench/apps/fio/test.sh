a2369bc548f4fb900d8260a5fb43259acf77755d
shaleen <shaleen@node-0.testing.lsm-pg0.utah.cloudlab.us>
2022-06-05 18:04:43 -0600

    multi thread test fio

--- appbench/apps/fio/test.sh
+++ appbench/apps/fio/test.sh
@@ -10,13 +10,21 @@ FlushDisk()
     sleep 2
 }
 
-SIZE=2g
-NUMJOBS=16
+declare -a nproc=("1" "2" "4" "8" "16")
+
+TOTSIZE=32 ##GB
 
 
-FlushDisk
+for NPROC in "${nproc[@]}"
+do
+        SIZE=`echo "$TOTSIZE/$NPROC" | bc`
+        NAME=NVME_${NPROC}
+
+        FlushDisk
+        fio --name=$NAME --directory=./fio-test --ioengine=psync --rw=read --bs=4k --numjobs=$NPROC --size=${SIZE}g --iodepth=1 --fadvise_hint=0 > out_$NAME
+done
+
 #export LD_PRELOAD=/usr/lib/libjuststats.so
-fio --name=nvme --directory=./fio-test --ioengine=psync --rw=read --bs=4k --numjobs=$NUMJOBS --size=$SIZE --iodepth=1 --fadvise_hint=0
 #fio --name=nvme --directory=./fio-test --time_based --size=10G --direct=0 --verify=0 --bs=4K --iodepth=1024 --rw=read --group_reporting=1 --numjobs=4
 #export LD_PRELOAD=""
 
