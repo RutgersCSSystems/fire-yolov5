656d97c370b3b96c182933194bbdec26d96992de
Shaleen Garg <shaleengarg.in@gmail.com>
2022-06-26 00:54:26 +0000

    test fio

--- appbench/apps/fio/test.sh
+++ appbench/apps/fio/test.sh
@@ -2,18 +2,18 @@
 
 FlushDisk()
 {
-    sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+    sudo sh -c "echo 1 > /proc/sys/vm/drop_caches"
     sudo sh -c "sync"
-    sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+    sudo sh -c "echo 1 > /proc/sys/vm/drop_caches"
     sudo sh -c "sync"
-    sudo dmesg --clear
+    #sudo dmesg --clear
     sleep 2
 }
 
 #declare -a nproc=("1" "2" "4" "8" "16")
 
-TOTSIZE=32 ##GB
-NPROC=8
+TOTSIZE=16 ##GB
+NPROC=2
 
 SIZE=`echo "$TOTSIZE/$NPROC" | bc`
 NAME=NVME_${NPROC}
@@ -38,7 +38,7 @@ FlushDisk
 free -h
 export LD_PRELOAD=""
 #strace -f $COMMAND
-#$COMMAND
+$COMMAND
 export LD_PRELOAD=""
 FlushDisk
 #vanilla
@@ -47,23 +47,18 @@ free -h
 
 echo "################################################################################"
 
-<< 'disable_adv'
-echo "DISABLE ADV.................."
-export LD_PRELOAD=/usr/lib/lib_DISABLE_ADV.so
-#ltrace -C -f -S -l /usr/lib/lib_DISABLE_ADV.so $COMMAND 2> ltrace_out_${TOTSIZE}_DISABLE
-$COMMAND
-export LD_PRELOAD=""
-FlushDisk
-disable_adv
-
+#echo "Vanilla RA.................."
 echo "Cross_Naive.................."
 FlushDisk
 #export LD_PRELOAD=/usr/lib/lib_Cross_Naive.so
 export LD_PRELOAD=/usr/lib/lib_CNI.so
+#export LD_PRELOAD=/usr/lib/lib_VRAI.so
 #ltrace -C -f -S -l /usr/lib/lib_Cross_Naive.so $COMMAND 2> ltrace_out_$TOTSIZE
 $COMMAND
 export LD_PRELOAD=""
 FlushDisk
+
+exit
 
 echo "Cross_Naive_IOOPT.................."
 FlushDisk
@@ -72,26 +67,7 @@ $COMMAND
 export LD_PRELOAD=""
 FlushDisk
 
-
-<< 'comment'
-for NPROC in "${nproc[@]}"
-do
-        SIZE=`echo "$TOTSIZE/$NPROC" | bc`
-        NAME=NVME_${NPROC}
-
-        FlushDisk
-        export LD_PRELOAD=/usr/lib/lib_INTERCEPT.so
-        #export LD_PRELOAD=/users/shaleen/ssd/prefetching/shared_libs/simple_prefetcher/lib_INTERCEPT.so
-        #ltrace -C -f -S -l /users/shaleen/ssd/prefetching/shared_libs/simple_prefetcher/lib_INTERCEPT.so fio --name=$NAME --directory=./fio-test --ioengine=psync --rw=read --bs=4k --numjobs=$NPROC --size=${SIZE}g --iodepth=1 --fadvise_hint=0 #> out_$NAME
-        fio --name=$NAME --directory=./fio-test --ioengine=psync --rw=read --bs=4k --numjobs=$NPROC --size=${SIZE}g --iodepth=1 --fadvise_hint=0 #> out_$NAME
-        export LD_PRELOAD=""
-        exit
-done
-comment
-
-
-#fio --name=nvme --directory=./fio-test --time_based --size=10G --direct=0 --verify=0 --bs=4K --iodepth=1024 --rw=read --group_reporting=1 --numjobs=4
-
+exit
 
 
 #fio can do strided reads 
