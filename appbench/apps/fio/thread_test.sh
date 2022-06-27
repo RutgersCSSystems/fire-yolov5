6e39dbb78c4d64eedae54d09eace1cb984637a01
shaleen <shaleen@node-0.fio-normal.lsm-pg0.utah.cloudlab.us>
2022-06-26 21:39:53 -0600

    fio testing vanilla

---
+++ appbench/apps/fio/thread_test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+##Tests fio read performance with increasing number of threads
+
+#set -x
+
+FlushDisk()
+{
+        sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+        sudo sh -c "sync"
+        sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+        sudo sh -c "sync"
+        sleep 5
+}
+
+FILESIZE=16
+
+declare -a nr_threads=("1" "2" "4" "8" "16")
+#declare -a readsize=("4" "2" "4" "8" "16") #in KB
+
+for NPROC in "${nr_threads[@]}"
+do
+	echo "######################################"
+        echo "$NPROC threads"
+	SIZE=`echo "$FILESIZE/$NPROC" | bc`
+	NAME=NVME_${NPROC}
+	COMMAND="fio --name=$NAME --directory=./fio-test --ioengine=psync --rw=read --bs=4k --numjobs=$NPROC --size=${SIZE}g --iodepth=1 --fadvise_hint=0 --thread"
+
+	FlushDisk
+	echo "VANILLA"
+	export LD_PRELOAD=""
+	$COMMAND | grep "READ"
+	FlushDisk
+
+<< 'MULTILINE-COMMENT'
+	FlushDisk
+	echo "#######"
+	echo "CNI"
+	export LD_PRELOAD=/usr/lib/lib_CNI.so
+	$COMMAND
+	export LD_PRELOAD=""
+	FlushDisk
+MULTILINE-COMMENT
+
+	exit
+done
+set +x
