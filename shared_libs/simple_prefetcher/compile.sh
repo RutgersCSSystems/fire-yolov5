63ebae57c34d3fb504347060919663c68fa085cf
shaleen <shaleen@node-0.fio-normal.lsm-pg0.utah.cloudlab.us>
2022-07-04 01:15:21 -0600

    compile library readme

--- shared_libs/simple_prefetcher/compile.sh
+++ shared_libs/simple_prefetcher/compile.sh
@@ -3,8 +3,12 @@
 ##This compile script will compile the preload libraries and install them
 
 #OS doesnt prefetch more than 256 pages in vanilla kernel
+
+# PREFETCH_SIZE should be greater than 64 pages else
+#there will be duplicate copy_to_user in readahead_info and
+# performance will go down.
 PREFETCH_SIZE=512 ##in nr of pages
-NR_WORKERS=4
+NR_WORKERS=8
 
 make NR_RA_PAGES=$PREFETCH_SIZE NR_WORKERS=$NR_WORKERS -j
 make install
