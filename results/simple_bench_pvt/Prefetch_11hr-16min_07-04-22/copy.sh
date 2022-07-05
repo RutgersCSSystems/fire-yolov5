aa2eb55ca055318eb6822a16952b3df248af5669
Shaleen Garg <shaleengarg.in@gmail.com>
2022-07-04 20:35:28 -0700

    copy data from this to plots folder

---
+++ results/simple_bench_pvt/Prefetch_11hr-16min_07-04-22/copy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+#This scripts only copies files from results folder to plots folder
+#No other work done here
+
+filesize=40 ##GB
+read_size=20 ## in pages
+nproc=16
+
+#Checks if the folder exits, if not, create new
+CREATE_OUTFOLDER() {
+        if [[ ! -e $1 ]]; then
+                mkdir -p $1
+        else
+                echo "$1 already exists"
+        fi
+}
+
+declare -a workload_arr=("read_pvt_rand" "read_pvt_seq") ##read binaries
+declare -a experiment=("VANILLA" "OSONLY" "CN" "CNI" "CPNV" "CPNI")
+
+
+OUTFILENAME="filesz-${filesize}_Readsz-${read_size}"
+for WORKLOAD in "${workload_arr[@]}"
+do
+        for EXPERIMENT in "${experiment[@]}"
+        do
+                OUTFOLDER=$PWD/plots/bandwidth_v_nproc/$WORKLOAD
+                CREATE_OUTFOLDER $OUTFOLDER
+
+                cp $PWD/$EXPERIMENT/$WORKLOAD/$OUTFILENAME $OUTFOLDER/${EXPERIMENT}_${WORKLOAD}.csv
+        done
+done
