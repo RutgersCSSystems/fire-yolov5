2a51f8608f3524d76dfbc64111f0519f5ac7f74c
kannan11 <kannan11@node-0.testbed-ra.lsm-pg0.utah.cloudlab.us>
2022-08-05 16:36:54 -0600

    test files

---
+++ appbench/apps/filebench/filebench-exp-test.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+DBHOME=$PWD
+THREAD=4
+VALUE_SIZE=4096
+SYNC=0
+KEYSIZE=1000
+WRITE_BUFF_SIZE=67108864
+NUM=1000000
+DBDIR=$DBHOME/DATA
+#APPPREFIX="sudo /usr/bin/time -v"
+APP="filebench"
+
+#WORKLOAD="readseq"
+#WORKLOAD="workloads/fileserver.f"
+#WORKLOAD="workloads/filemicro_seqread.f"
+WORKLOAD="workloads/filemicro_rread.f"
+WRITEARGS="-f $WORKLOAD"
+READARGS="-f $WORKLOAD"
+RESULTS=$OUTPUTDIR/$APP/$WORKLOAD
+
+
+mkdir -p $RESULTS
+
+declare -a config_arr=("Vanilla" "Cross_Naive" "CPBI" "CPNI" "CNI" "CPBV" "CPNV")
+#declare -a config_arr=("Cross_Naive" "CPBI" "CPNI" "CNI" "CPBV" "CPNV")
+#declare -a config_arr=("Vanilla" "CPNI")
+
+#declare -a workload_arr=("filemicro_seqwrite.f" "mongo.f" "videoserver.f" "fileserver.f" "randomrw.f" "randomread.f")
+declare -a workload_arr=("oltp.f")
+
+
+
+FlushDisk()
+{
+        sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+        sudo sh -c "sync"
+        sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+        sudo sh -c "sync"
+        sudo dmesg --clear
+        sleep 5
+}
+
+
+CLEAR_DATA()
+{
+	sudo killall $APP
+	sudo killall $APP
+	sleep 3
+	sudo killall $APP
+        rm -rf $DBDIR/*
+}
+
+
+CLEAN_AND_WRITE()
+{
+        #printf "in ${FUNCNAME[0]}\n"
+        export LD_PRELOAD=""
+        CLEAR_DATA
+        FlushDisk
+}
+
+print_results() {
+
+	echo "Vanilla Results"
+	cat $RESULTS/VANILLA.out | grep "$WORKLOAD" | awk '{print $7}'
+	echo "...."
+
+	echo "CPNI Results"
+	cat $RESULTS/CPNI.out | grep "$WORKLOAD" | awk '{print $7}'
+	echo "...."
+
+	echo "CNI Results"
+	cat $RESULTS/CNI.out | grep "$WORKLOAD" | awk '{print $7}'
+	echo "...."
+
+	echo "CPBI Results"
+	cat $RESULTS/CPBI.out | grep "$WORKLOAD" | awk '{print $7}'
+	echo "...."
+
+	echo "CPBV Results"
+	cat $RESULTS/CPBV.out | grep "$WORKLOAD" | awk '{print $7}'
+	echo "...."
+
+	echo "CPNV Results"
+	cat $RESULTS/CPNV.out | grep "$WORKLOAD" | awk '{print $7}'
+	echo "...."
+
+	echo "CROSS-NAIVE Results"
+	cat $RESULTS/Cross_Naive.out | grep "$WORKLOAD" | awk '{print $7}'
+	echo "...."
+
+}
+
+
+RUN() {
+
+	for CONFIG in "${config_arr[@]}"
+	do
+
+		for WORKLOAD in "${workload_arr[@]}"
+		do
+			RESULTS=""
+			WORKLOAD="workloads/$WORKLOAD"
+			WRITEARGS="-f $WORKLOAD"
+			READARGS="-f $WORKLOAD"
+			RESULTS=$OUTPUTDIR/$APP/$WORKLOAD
+
+			mkdir -p $RESULTS
+
+			echo "For Workload $WORKLOAD, generating $RESULTS/$CONFIG.out"
+
+			#echo "BEGINNING TO WARM UP ......."
+			CLEAN_AND_WRITE
+			#echo "FINISHING WARM UP ......."
+			echo "..................................................."
+			FlushDisk
+
+			echo "RUNNING $CONFIG...................................."
+			echo "..................................................."
+			export LD_PRELOAD=/usr/lib/lib_$CONFIG.so
+			$APPPREFIX $APP $PARAMS $READARGS &> $RESULTS/$CONFIG.out
+			export LD_PRELOAD=""
+			FlushDisk
+			echo ".......FINISHING $CONFIG......................"
+			CLEAR_DATA
+			FlushDisk
+		done
+	done
+}
+
+RUN
+#print_results
+exit
