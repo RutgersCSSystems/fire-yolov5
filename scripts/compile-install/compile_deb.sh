6efb42d99eca33a2bce45c439b253376bde4eb48
kannan11 <kannan11@node-0.testbed-ra.lsm-pg0.utah.cloudlab.us>
2022-08-08 12:11:47 -0600

    fixing kernel compile script issues

--- scripts/compile-install/compile_deb.sh
+++ scripts/compile-install/compile_deb.sh
@@ -4,6 +4,13 @@ sudo apt-get install -y libdpkg-dev kernel-package libncurses-dev
 export CONCURRENCY_LEVEL=40
 export CONCURRENCYLEVEL=40
 touch REPORTING-BUGS
+
+#Disable them
+scripts/config --disable SYSTEM_REVOCATION_KEYS
+scripts/config --disable SYSTEM_TRUSTED_KEYS
+
+
+
 #mv .config .config_back
 #make distclean
 #sudo make menuconfig
