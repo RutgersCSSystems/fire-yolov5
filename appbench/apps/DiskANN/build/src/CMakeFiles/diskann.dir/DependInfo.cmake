5853f0c9a00d46fced5d662f4b5e63541d9a8c42
sudarsun <sudarsun>
2022-08-18 12:03:46 -0400

    Application: Adding disk-based Augmented Neural Net app

---
+++ appbench/apps/DiskANN/build/src/CMakeFiles/diskann.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/burger/code/DiskANN/src/ann_exception.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/ann_exception.cpp.o"
+  "/home/burger/code/DiskANN/src/aux_utils.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/aux_utils.cpp.o"
+  "/home/burger/code/DiskANN/src/distance.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/distance.cpp.o"
+  "/home/burger/code/DiskANN/src/index.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/index.cpp.o"
+  "/home/burger/code/DiskANN/src/linux_aligned_file_reader.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/linux_aligned_file_reader.cpp.o"
+  "/home/burger/code/DiskANN/src/logger.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/logger.cpp.o"
+  "/home/burger/code/DiskANN/src/math_utils.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/math_utils.cpp.o"
+  "/home/burger/code/DiskANN/src/memory_mapper.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/memory_mapper.cpp.o"
+  "/home/burger/code/DiskANN/src/natural_number_map.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/natural_number_map.cpp.o"
+  "/home/burger/code/DiskANN/src/natural_number_set.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/natural_number_set.cpp.o"
+  "/home/burger/code/DiskANN/src/partition_and_pq.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/partition_and_pq.cpp.o"
+  "/home/burger/code/DiskANN/src/pq_flash_index.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/pq_flash_index.cpp.o"
+  "/home/burger/code/DiskANN/src/utils.cpp" "/home/burger/code/DiskANN/build/src/CMakeFiles/diskann.dir/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MKL_ILP64"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/intel/oneapi/mkl/latest/include"
+  "/usr/include/mkl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
