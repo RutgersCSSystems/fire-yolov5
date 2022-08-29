94f5566a726f3b07cce533cc855003fd35ba0793
shaleen <shaleen@node-0.testbed.lsm-pg0.utah.cloudlab.us>
2022-08-29 10:57:42 -0600

    run.sh updated

--- appbench/apps/simple_bench/multi_thread_read/run.sh
+++ appbench/apps/simple_bench/multi_thread_read/run.sh
@@ -7,10 +7,15 @@ FlushDisk()
     sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
     sudo sh -c "sync"
     sleep 5
+    sudo dmesg --clear
 }
 
 
 FlushDisk
-export LD_PRELOAD="/usr/lib/lib_CII.so"
+
+#export LD_PRELOAD="/usr/lib/lib_CII.so"
+export LD_PRELOAD="/usr/lib/lib_Cross_Info.so"
 ./bin/read_shared_seq
 export LD_PRELOAD=""
+
+dmesg
