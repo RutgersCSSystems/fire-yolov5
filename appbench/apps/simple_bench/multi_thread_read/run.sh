f59c8772c8ce5fd319c0d4f6e59571fa8fcadd9c
shaleen <shaleen@node-0.statsrun.lsm-pg0.utah.cloudlab.us>
2022-08-31 07:43:54 -0600

    run.sh changes

--- appbench/apps/simple_bench/multi_thread_read/run.sh
+++ appbench/apps/simple_bench/multi_thread_read/run.sh
@@ -10,15 +10,31 @@ FlushDisk()
     sudo dmesg --clear
 }
 
+ENABLE_LOCK_STATS()
+{
+	sudo sh -c "echo 0 > /proc/lock_stat"
+	sudo sh -c "echo 1 > /proc/sys/kernel/lock_stat"
+}
+
+DISABLE_LOCK_STATS()
+{
+	sudo sh -c "echo 0 > /proc/sys/kernel/lock_stat"
+}
+
 
 FlushDisk
 
+ENABLE_LOCK_STATS
 #export LD_PRELOAD="/usr/lib/lib_CII.so"
 #export LD_PRELOAD="/usr/lib/lib_VRA.so"
-#export LD_PRELOAD="/usr/lib/lib_Cross_Info_sync.so"
 #export LD_PRELOAD="/usr/lib/lib_CII_sync.so"
-export LD_PRELOAD="/usr/lib/lib_VRA.so"
+#export LD_PRELOAD="/usr/lib/lib_Cross_Info.so"
+#export LD_PRELOAD="/usr/lib/lib_Cross_Info_sync.so"
+#export LD_PRELOAD="/usr/lib/lib_Cross_Info.so"
+export LD_PRELOAD="/usr/lib/lib_CII.so"
 ./bin/read_shared_seq
 export LD_PRELOAD=""
+DISABLE_LOCK_STATS
 
 dmesg
+sudo cat /proc/lock_stat
