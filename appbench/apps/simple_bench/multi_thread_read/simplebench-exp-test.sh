c472a14cc7d0d08fb81f7e9fd8c3f9c46a808ecc
shaleen <shaleen@node-0.statsrun.lsm-pg0.utah.cloudlab.us>
2022-08-31 07:44:14 -0600

    one in all script for simplebench

---
+++ appbench/apps/simple_bench/multi_thread_read/simplebench-exp-test.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+#set +x
+
+if [ -z "$APPS" ]; then
+        echo "APPS environment variable is undefined."
+        echo "Did you setvars? goto Base directory and $ source ./scripts/setvars.sh"
+        exit 1
+fi
+
+##This script would run strided MADBench and collect its results
+source $RUN_SCRIPTS/generic_funcs.sh
+
+base=$APPS/simple_bench/multi_thread_read
+
+APPOUTPUTNAME="SIMPLEBENCH"
+
+RESULTS="RESULTS"/$WORKLOAD
+RESULTFILE=""
+
+declare -a nproc=("1" "2" "4" "8" "16")
+declare -a read_size=("20") ## in pages
+declare -a workload_arr=("read_shared_seq") ##read binaries
+declare -a config_arr=("Vanilla" "VRA" "Cross_Info" "CII")
+
+STATS=1 #0 for perf runs and 1 for stats
+NR_STRIDE=64 ##In pages, only relevant for strided
+FILESIZE=80 ##GB
+
+#Compiles the application
+COMPILE_APP() {
+        pushd $base
+        CREATE_OUTFOLDER $base/bin
+        make -j SIZE=$1 NR_READ_PAGES=$2 NR_THREADS=$3 NR_STRIDE=$NR_STRIDE
+        popd
+}
+
+#deletes all the Read files
+CLEAR_FILES() {
+        pushd $base
+        rm -rf ./threads_*/
+        popd
+}
+
+#takes Workload and filesize
+CLEAN_AND_WRITE() {
+        printf "in ${FUNCNAME[0]}\n"
+
+        UNSETPRELOAD
+
+        pushd $base
+
+        if [[ "$1" == *"shared"* ]]; then
+                echo "Shared File"
+                FILENAME="./threads_1/bigfakefile0.txt"
+                FILESZ=$(stat -c %s $FILENAME)
+                FILESIZE_WANTED=`echo "$2*$GB" | bc`
+
+                if [ "$FILESZ" -ne "$FILESIZE_WANTED" ]; then
+                        CLEAR_FILES
+                        $base/bin/write_shared
+                fi
+        else
+                echo "Pvt Files"
+                CLEAR_FILES
+                $base/bin/write_pvt
+        fi
+
+        popd
+
+        FlushDisk
+}
+
+GEN_RESULT_PATH() {
+        WORKLOAD=$1
+        CONFIG=$2
+        THREAD=$3
+        if [ "$STATS" -eq "1" ]; then
+                RESULTS=$OUTPUTDIR/${APPOUTPUTNAME}_STATS/$WORKLOAD/$THREAD
+        else
+                RESULTS=$OUTPUTDIR/${APPOUTPUTNAME}_PERF/$WORKLOAD/$THREAD
+        fi
+        mkdir -p $RESULTS
+        RESULTFILE=$RESULTS/$CONFIG.out
+}
+
+
+RUN() {
+        echo "STARTING to RUN"
+
+        for WORKLOAD in "${workload_arr[@]}"
+        do
+                for NPROC in "${nproc[@]}"
+                do
+                        for READ_SIZE in "${read_size[@]}"
+                        do
+                                COMPILE_APP $FILESIZE $READ_SIZE $NPROC
+                                CLEAN_AND_WRITE $WORKLOAD $FILESIZE
+
+                                for CONFIG in "${config_arr[@]}"
+                                do
+
+                                        echo "######################################################,"
+                                        echo "Filesize=$FILESIZE, load=$WORKLOAD, Experiment=$experiment NPROC=$NPROC Readsz=$READ_SIZE"
+
+                                        GEN_RESULT_PATH $WORKLOAD $CONFIG $NPROC
+
+                                        if [ "$STATS" -eq "1" ]; then
+                                                ENABLE_LOCK_STATS
+                                        fi
+
+                                        export LD_PRELOAD=/usr/lib/lib_$CONFIG.so
+					$base/bin/$WORKLOAD &> $RESULTFILE
+					export LD_PRELOAD=""
+
+                                        if [ "$STATS" -eq "1" ]; then
+                                                DISABLE_LOCK_STATS
+                                        fi
+
+					sudo dmesg -c &>> $RESULTFILE
+                                        sudo cat /proc/lock_stat &>> $RESULTFILE
+
+                                        REFRESH
+                                done
+                        done
+                done
+        done
+}
+
+RUN
