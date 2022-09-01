0ef22c39a43ebbf6fb106f681cac967657a04297
sudarsun <sudarsun>
2022-09-01 12:24:25 -0400

    graph changes

--- scripts/graphs/plot.sh
+++ scripts/graphs/plot.sh
@@ -5,4 +5,10 @@ export legendlist="Vanilla,VanillaOPT,OSonly,CrossInfo,CrossInfoIOOPT"
 export legendnamelist="Vanilla,VanillaOPT,OSonly,CrossInfo,CrossInfoIOOPT"
 export ymax=40
 export yinterval=5
+
+#default y-axis and x-axis title
+export ytitledef='Throughput (MB/sec)'
+export xtitledef="#.of threads"
+
+
 python plot.py graph.DATA outputgraph
