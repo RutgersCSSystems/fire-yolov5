de558db0408986c614b401c5344fa81778febc3e
sudarsun <sudarsun.kannan@rutgers.edu>
2022-09-02 20:10:00 +0000

    more packages

--- appbench/apps/filebench/compile.sh
+++ appbench/apps/filebench/compile.sh
@@ -2,11 +2,16 @@
 sudo apt update
 sudo apt-get install libtool -y
 sudo apt-get install bison -y
+sudo apt install flex -y
+alias yacc="bison"
+sudo ln -s /bin/bison /bin/yacc
 libtoolize
 aclocal
 autoheader
 automake --add-missing
 autoconf
+
+
 
 ./configure
 
