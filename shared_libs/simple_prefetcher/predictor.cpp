89e7d38847be8999fd1a041b5f220328705d6fbc
Shaleen Garg <shaleengarg.in@gmail.com>
2022-09-08 16:49:08 -0400

    modular

--- shared_libs/simple_prefetcher/predictor.cpp
+++ shared_libs/simple_prefetcher/predictor.cpp
@@ -40,7 +40,133 @@ void hello_predictor(){
         return;
 }
 
+file_predictor::file_predictor(int this_fd, size_t size){
 
+        fd = this_fd;
+        filesize = size;
+
+
+        portion_sz = PAGESIZE * PORTION_PAGES;
+        nr_portions = size/portion_sz;
+
+        //Imperfect division of filesize with portion_sz
+        //add one bit to accomodate for the last portion in file
+        if(size % portion_sz){
+                nr_portions += 1;
+        }
+
+        access_history = BitArrayCreate(nr_portions);
+        BitArrayClearAll(access_history);
+
+#if defined(READAHEAD_INFO_PC_STATE) && !defined(PER_INODE_BITMAP)
+        page_cache_state = BitArrayCreate(NR_BITS_PREALLOC_PC_STATE);
+        BitArrayClearAll(page_cache_state);
+#else
+        page_cache_state = NULL;
+#endif
+
+        //Assume any opened file is probably not sequential
+        sequentiality = POSSNSEQ;
+        stride = 0;
+        read_size = 0;
+}
+
+file_predictor::~file_predictor(){
+        BitArrayDestroy(access_history);
+
+#ifdef READAHEAD_INFO_PC_STATE
+        BitArrayDestroy(page_cache_state);
+#endif
+}
+
+/*
+ * If offset being accessed is from an Unset file portion, set it,
+ * 1. Set that file portion in access_history
+ * 2. reduce the sequentiality
+ *
+ * else increase the sequentiality
+ *
+ */
+void file_predictor::predictor_update(off_t offset, size_t size){
+
+#if 0
+        /*
+         * Once the file is sent for prefetching
+         * there is no point in keeping the prefetcher running for this file
+         * TODO: test only works for C++20
+         */
+        if(already_prefetched.test()){
+                goto exit;
+        }
+#endif
+
+
+        size_t portion_num = offset/portion_sz; //which portion
+        size_t num_portions = size/portion_sz; //how many portions in this req
+        size_t pn = 0; //used for adjacency check
+
+        if(portion_num > nr_portions){
+                printf("%s: ERR : portion_num > nr_portions, has the filesize changed ?\n", __func__);
+                goto exit;
+        }
+
+        /*
+         * Go through the bit array, setting ones portions associated
+         * with this read request
+         */
+        for(long i=0; i<=num_portions; i++)
+                BitArraySetBit(access_history, portion_num+i);
+
+        /*
+         * Determine if this sequential or strided
+         * TODO: Convert this to a bit operation, this is heavy
+         * Develop a bit mask and test the corresponding bits
+         */
+        for(long i = 1; i <= NR_ADJACENT_CHECK; i++){
+
+                pn = portion_num - i;
+
+                /*bounds check*/
+                if((long)pn < 0){
+                        goto exit;
+                }
+
+                if(BitArrayTestBit(access_history, pn)){
+                        stride = portion_num - pn - 1;
+                        if(stride > 0)
+                                read_size = size;
+                        //debug_printf("%s: stride=%ld\n", __func__, stride);
+                        goto is_seq;
+                }
+
+        }
+
+is_not_seq:
+        //sequentiality -= 1;
+        //sequentiality %= (DEFNSEQ-1); //Keeps from underflowing
+        sequentiality = (std::max<long>)(DEFNSEQ, sequentiality-1); //keeps from underflowing
+        goto exit;
+
+is_seq:
+        //if(sequentiality < DEFSEQ)
+        //sequentiality += 1;
+        //sequentiality %= (DEFSEQ+1); //Keeps from overflowing
+        sequentiality = (std::min<long>)(DEFSEQ, sequentiality+1); //keeps from overflowing
+
+exit:
+        return;
+}
+
+//Returns the current Sequentiality value
+long file_predictor::is_sequential(){
+        return sequentiality;
+}
+
+//returns the approximate stride in pages
+//0 if not strided. doesnt mean its not sequential
+long file_predictor::is_strided(){
+        return stride*PORTION_PAGES;
+}
 
 
 #endif //PREDICTOR
