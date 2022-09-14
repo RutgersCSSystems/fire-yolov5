9cfa7d7018387b425ae542c9b59988f292738f78
sudarsun <sudarsun@heterosys.cs.rutgers.edu>
2022-09-14 12:17:58 -0400

    optimizaiton

--- shared_libs/simple_prefetcher/frontend.cpp
+++ shared_libs/simple_prefetcher/frontend.cpp
@@ -762,9 +762,9 @@ void inline record_open(struct file_desc desc){
 		debug_printf("%s: first READAHEAD: %ld\n", __func__, ptd.mytid);
 		//clock_gettime(CLOCK_REALTIME, &start);
 
-	        struct read_ra_req ra;
-		ra.data = NULL;
-		readahead_info(fd, 0, 0, &ra);
+	    //struct read_ra_req ra;
+		//ra.data = NULL;
+		//readahead_info(fd, 0, 0, &ra);
 
 		//debug_printf("%s: DONE first READAHEAD: %ld in %lf microsec new\n", __func__, ptd.mytid, get_micro_sec(&start, &end));
 		debug_printf("%s: DONE first READAHEAD: %ld\n", __func__, ptd.mytid);
