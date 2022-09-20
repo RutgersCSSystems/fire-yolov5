3973d2d76482651f61b3187e09c6bdea20c7732b
sudarsun <sudarsun@heterosys.cs.rutgers.edu>
2022-09-20 14:32:23 -0400

    refactor-sudarsun

--- shared_libs/simple_prefetcher/predictor.cpp
+++ shared_libs/simple_prefetcher/predictor.cpp
@@ -200,8 +200,7 @@ bool file_predictor::should_prefetch_now(){
 
         debug_printf("%s: fd=%d last_read_offset=%ld, last_ra_offset=%ld, diff=%ld, prefetch_limit=%ld\n",
                         __func__, fd, last_read_offset, last_ra_offset, (last_ra_offset-last_read_offset), prefetch_limit);
-
-        if(prefetch_limit > 0 && !uinode->fully_prefetched.load()){
+        if(prefetch_limit > 0 && this->uinode && (this->uinode->ino > 0) && !this->uinode->fully_prefetched.load()){
                 return true;
         }
 
