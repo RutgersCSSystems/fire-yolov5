fdab48b89a8e7886e5333bb14bc382948a56655f
shaleen <shaleen@node-0.statsrun.lsm-pg0.utah.cloudlab.us>
2022-09-21 07:15:31 -0600

    memlimit experiment rocksdb

--- appbench/apps/rocksdb/simple_memlimit.sh
+++ appbench/apps/rocksdb/simple_memlimit.sh
@@ -8,6 +8,7 @@ fi
 
 ##This script would run strided MADBench and collect its results
 source $RUN_SCRIPTS/generic_funcs.sh
+APPPREFIX="/usr/bin/time -v"
 
 ulimit -n 1000000
 
@@ -17,16 +18,18 @@ VALUE_SIZE=4096
 SYNC=0
 KEYSIZE=1000
 WRITE_BUFF_SIZE=67108864
-NUM=40000000
+NUM=20000000
 DBDIR=$DBHOME/DATA
 base=$PWD
 
 slack_MB=1024
+
 MEM_BUDGET_PER='0.5'
 
-declare -a num_arr=("40000000")
+declare -a num_arr=("20000000")
+declare -a config_arr=("Vanilla" "OSonly" "Cross_Info_sync" "CIPB")
 
-WORKLOAD="readseq"
+WORKLOAD="multireadrandom"
 #WORKLOAD="readrandom"
 #WORKLOAD="readreverse"
 #READARGS="--benchmarks=$WORKLOAD --use_existing_db=1 --mmap_read=0 --threads=$THREAD"
@@ -58,29 +61,27 @@ CLEAN_AND_WRITE()
         FlushDisk
 
         ##Condition the DB to get Stable results
-	$DBHOME/db_bench $PARAMS $ORI_READARGS --benchmarks=readseq --threads=16
+        $DBHOME/db_bench $PARAMS $ORI_READARGS --benchmarks=readseq --threads=16
         FlushDisk
-	$DBHOME/db_bench $PARAMS $ORI_READARGS --benchmarks=readseq --threads=16
+        $DBHOME/db_bench $PARAMS $ORI_READARGS --benchmarks=readseq --threads=16
         FlushDisk
 }
 
 for NUM in "${num_arr[@]}"
 do
-        PARAMS="--db=$DBDIR --value_size=$VALUE_SIZE --wal_dir=$DBDIR/WAL_LOG --sync=$SYNC --key_size=$KEYSIZE --write_buffer_size=$WRITE_BUFF_SIZE --num=$NUM"
+        PARAMS="--db=$DBDIR --value_size=$VALUE_SIZE --wal_dir=$DBDIR/WAL_LOG --sync=$SYNC --key_size=$KEYSIZE --write_buffer_size=$WRITE_BUFF_SIZE --num=$NUM  --seed=1576170874"
 
-        CLEAN_AND_WRITE
+        #CLEAN_AND_WRITE
         FlushDisk
 
         printf "\nRUNNING Memlimit.................\n"
-        #SETPRELOAD "VANILLA"
         export LD_PRELOAD=/usr/lib/lib_memusage.so
-        #$DBHOME/db_bench $PARAMS $READARGS
-	$DBHOME/db_bench $PARAMS $ORI_READARGS --benchmarks=readseq --threads=16 &> out_memusage
+        $DBHOME/db_bench $PARAMS $ORI_READARGS --benchmarks=readseq --threads=16 &> out_memusage
         export LD_PRELOAD=""
         FlushDisk
 
-	total_anon_MB=`cat out_memusage | grep "total_anon_used" | awk '{print $2}'`
-	total_cache_MB=`cat out_memusage | grep "total_anon_used" | awk '{print $5}'`
+        total_anon_MB=`cat out_memusage | grep "total_anon_used" | awk '{print $2}'`
+        total_cache_MB=`cat out_memusage | grep "total_anon_used" | awk '{print $5}'`
 
         echo "total_anon_MB = $total_anon_MB"
         echo "total_cache_MB = $total_cache_MB"
@@ -89,17 +90,20 @@ do
         SETUPEXTRAM_1 `echo "scale=0; (($total_anon_MB + ($total_cache_MB*$MEM_BUDGET_PER))+$slack_MB)/1" | bc --mathlib`
         free -h
 
-	READARGS="$ORI_READARGS --benchmarks=$WORKLOAD --threads=$THREAD"
+        for CONFIG in "${config_arr[@]}"
+        do
+                printf "\nrunning $CONFIG MEM=$MEM_BUDGET_PER WORKLOAD=$WORKLOAD NUM=$NUM THREADS=$THREAD....\n"
 
+                READARGS="$ORI_READARGS --benchmarks=$WORKLOAD --threads=$THREAD"
 
-        RESULTFILE=stats_${WORKLOAD}_${THREAD}_${total_anon_MB}_${total_cache_MB}_${MEM_BUDGET_PER}
+                RESULTFILE="mb_${WORKLOAD}_${CONFIG}_${THREAD}_${NUM}_${MEM_BUDGET_PER}"
 
-        printf "\nOSOnly....\n"
-        export LD_PRELOAD=/usr/lib/lib_OSonly.so
-        $DBHOME/db_bench $PARAMS $READARGS &> $RESULTFILE
-        export LD_PRELOAD=""
-	sudo dmesg -c &>> $RESULTFILE
-        FlushDisk
+		export LD_PRELOAD=/usr/lib/lib_$CONFIG.so
+                $APPPREFIX $DBHOME/db_bench $PARAMS $READARGS &> $RESULTFILE
+                export LD_PRELOAD=""
+                sudo dmesg -c &>> $RESULTFILE
+                FlushDisk
+        done
 
         umount_ext4ramdisk
 done
