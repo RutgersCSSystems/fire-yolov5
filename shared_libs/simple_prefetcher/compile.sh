9d4b1866138481e22774aca008f5e42e70d41149
shaleen <shaleen@node-0.r6526-nvme.lsm-pg0.clemson.cloudlab.us>
2022-09-22 05:17:03 -0400

    compile script for simpleprefetcher

--- shared_libs/simple_prefetcher/compile.sh
+++ shared_libs/simple_prefetcher/compile.sh
@@ -10,7 +10,7 @@
 
 #sudo apt update; sudo apt install mpich -y
 
-PREFETCH_SIZE=1024 ##in nr of pages
+PREFETCH_SIZE=4096 ##in nr of pages
 NR_WORKERS=4
 CROSS_BITMAP_SHIFT=37
 NR_PREDICT_SAMPLE_FREQ=8
