d02b0f624b8fa299af4c5b1eadafb8a9b91082ec
sudarsun <sudarsun@heterosys.cs.rutgers.edu>
2022-09-27 11:54:14 -0400

    prefetching

--- shared_libs/simple_prefetcher/uinode.cpp
+++ shared_libs/simple_prefetcher/uinode.cpp
@@ -240,8 +240,9 @@ int add_fd_to_inode(struct hashtable *i_map, int fd){
        */
 		uinode->page_cache_state = BitArrayCreate(NR_BITS_PREALLOC_PC_STATE);
         BitArrayClearAll(uinode->page_cache_state);
-        printf("%s: adding page cache to uinode %d with %u bits\n",
+        printf("%s: adding page cache to uinode %d with %lu bits\n",
         		__func__, inode, NR_BITS_PREALLOC_PC_STATE);
+
 #else
                 uinode->page_cache_state = NULL;
 #endif
