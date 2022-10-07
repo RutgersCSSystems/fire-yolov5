94d840f3a9a6dd07256da3d1749e9584c2e8a12b
sudarsun <kannan11@node-0.clemson1.lsm-pg0.clemson.cloudlab.us>
2022-10-07 12:30:26 -0400

    adding new node experiments

--- appbench/apps/simple_bench/multi_thread_read/simplebench-exp-test.sh
+++ appbench/apps/simple_bench/multi_thread_read/simplebench-exp-test.sh
@@ -30,6 +30,7 @@ declare -a workload_arr=("read_shared_rand_simple" "read_shared_seq_global_simpl
 
 declare -a nproc=("16")
 declare -a readsize_arr=("4" "128")
+declare -a readsize_arr=("32")
 declare -a workload_arr=("read_pvt_rand" "read_shared_rand" "read_shared_seq" "read_pvt_seq") ##read binaries
 declare -a config_arr=("Vanilla" "Cross_Info" "CII" "CIP" "CIPI" "OSonly")
 #declare -a config_arr=("Cross_Info" "CII" "CIP" "CIPI" "OSonly")
--- results/extract-result-update.sh
+++ results/extract-result-update.sh
@@ -88,11 +88,12 @@ set_simplebench_global_vars() {
 
 set_simplebench_read_size_sensitivity_global_vars() {
 
-	#simplebenchworkarr=("read_shared_seq-READSIZE-4" "read_shared_seq-READSIZE-128" "read_shared_rand-READSIZE-4" "read_shared_rand-READSIZE-128" "read_pvt_seq-READSIZE-4" "read_pvt_seq-READSIZE-128"  "read_pvt_rand-READSIZE-4" "read_pvt_rand-READSIZE-128")
-	#simplebenchproxyarr=("shareseq-4" "shareseq-128" "sharerand-4" "sharerand-128" "privseq-4" "privseq-128" "privrand-4" "privrand-128")
-
 	simplebenchworkarr=("read_pvt_seq-READSIZE-4" "read_pvt_seq-READSIZE-128" "read_pvt_rand-READSIZE-4" "read_pvt_rand-READSIZE-128" "read_shared_seq-READSIZE-4" "read_shared_seq-READSIZE-128" "read_shared_rand-READSIZE-4" "read_shared_rand-READSIZE-128")
 	simplebenchproxyarr=("privseq-4" "privseq-128" "privrand-4" "privrand-128" "shareseq-4" "shareseq-128" "sharerand-4" "sharerand-128")
+
+	simplebenchworkarr=("read_pvt_seq-READSIZE-4" "read_pvt_seq-READSIZE-32" "read_pvt_rand-READSIZE-4" "read_pvt_rand-READSIZE-32" "read_shared_seq-READSIZE-4" "read_shared_seq-READSIZE-32" "read_shared_rand-READSIZE-4" "read_shared_rand-READSIZE-32")
+	simplebenchproxyarr=("privseq-4" "privseq-32" "privrand-4" "privrand-32" "shareseq-4" "shareseq-32" "sharerand-4" "sharerand-32")
+
 
 	threadarr=("16")
 }
