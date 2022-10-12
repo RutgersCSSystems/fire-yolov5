d27a70a973bac498c5ed8fe26fc6a379087c3835
sudarsun <sudarsun@heterosys.cs.rutgers.edu>
2022-10-12 14:05:20 -0400

    debugging

--- shared_libs/simple_prefetcher/frontend.cpp
+++ shared_libs/simple_prefetcher/frontend.cpp
@@ -1140,9 +1140,10 @@ void update_file_predictor_and_prefetch(void *arg){
 #endif
 		if(fp->should_prefetch_now()){
 			a->fp = fp;
-			prefetch_file_predictor(arg);
+			(arg);
 		    //threadpool_add(pool[g_next_queue % QUEUES], prefetch_file_predictor, (void*)arg, 0);
 		    //g_next_queue++;
+			prefetch_file_predictor((void*)arg);
 		}
 	}
 }
