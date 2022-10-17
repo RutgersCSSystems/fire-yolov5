9c9b2e629169e1f0e558d3e5dd2f36a2b1d665dc
sudarsun <kannan11@node-0.clemson1.lsm-pg0.clemson.cloudlab.us>
2022-10-16 22:30:49 -0400

    adding reduce and release scripts

---
+++ scripts/mount/releasemem.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+#script to create and mount a pmemdir
+#requires size as input
+#xfs
+sudo umount /mnt/ext4ramdisk
+sudo rm -rf /mnt/ramdisk/ext4.image
+sudo umount /mnt/ramdisk
+rm -rf $SHARED_DATA
+mkdir $SHARED_DATA
