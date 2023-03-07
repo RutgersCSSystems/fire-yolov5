c82e09518c54fbdb6f170f631428729a6ada899a
kannan11 <kannan11@node0.prefetch1.lsm-pg0.clemson.cloudlab.us>
2023-03-07 16:10:24 -0500

    adding current results for ROCKSDB

--- scripts/sk2113-setvars.sh
+++ scripts/sk2113-setvars.sh
@@ -22,7 +22,7 @@ export PAPER=$NVMBASE/IntelliOS-paper/crossprefetch/
 export PAPERGRAPHS=$NVMBASE/IntelliOS-paper/crossprefetch/graphs/OCT7
 
 #BENCHMARKS AND LIBS
-export OUTPUT_FOLDER=$NVMBASE/results/SC-RESULTS-TRIAL1
+export OUTPUT_FOLDER=$NVMBASE/results/SC-RESULTS-TRIAL4
 
 export OUTPUTDIR=$OUTPUT_FOLDER
 export LINUX_SCALE_BENCH=$NVMBASE/linux-scalability-benchmark
