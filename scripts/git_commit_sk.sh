00b689db9f1ca5f8376caf8b821ec38dd94bf2da
sudarsun <kannan11@node-0.prefetch10.lsm-pg0.clemson.cloudlab.us>
2023-09-25 17:28:08 -0400

    perf opts

--- scripts/git_commit_sk.sh
+++ scripts/git_commit_sk.sh
@@ -2,4 +2,4 @@
 set -x
 git pull https://github.com/RutgersCSSystems/ioopt refactor-sudarsun-perf-4
 git commit -am "$1"
-git push origin https://github.com/RutgersCSSystems/ioopt refactor-sudarsun-perf-4
+git push origin #https://github.com/RutgersCSSystems/ioopt refactor-sudarsun-perf-4
