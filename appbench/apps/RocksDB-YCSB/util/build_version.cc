492a882acf383af5eb8c1befd465bbe3bf6f5999
kannan11 <kannan11@node-0.prefetch17.lsm-pg0.clemson.cloudlab.us>
2023-10-03 15:51:12 -0400

    fixes

--- appbench/apps/RocksDB-YCSB/util/build_version.cc
+++ appbench/apps/RocksDB-YCSB/util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:94b5da6258bb358421b74dea819d8f0ddeb36b97";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:6fe15d57d14ef1d3d14700a262b115525c896968";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:2023-10-03";
 const char* rocksdb_build_compile_date = __DATE__;
