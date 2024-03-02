1740f39a1aa400f909ce2dca5ec837b83d333159
kannan11 <kannan11@node-0.hotstorage23.lsm-pg0.clemson.cloudlab.us>
2024-03-02 22:54:57 +0000

    adding <sys/sysmacros.h>

--- appbench/apps/RocksDB-YCSB/util/build_version.cc
+++ appbench/apps/RocksDB-YCSB/util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:c402cc85270373b4a6878cc611b1456bcb384043";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:6825721f4bfa9cf59afb411359d62992d2b88ff6";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:2024-03-02";
 const char* rocksdb_build_compile_date = __DATE__;
