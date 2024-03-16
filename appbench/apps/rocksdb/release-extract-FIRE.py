ea3cfee150dcda75858129eb7f6df2e62fb4020e
sudarsun <kannan11@node-0.prefetch30.lsm-pg0.clemson.cloudlab.us>
2024-03-16 16:28:08 -0400

    fixes

--- appbench/apps/rocksdb/release-extract-FIRE.py
+++ appbench/apps/rocksdb/release-extract-FIRE.py
@@ -45,7 +45,7 @@ def plot_access_pattern(datafile, access_pattern, result_path):
                             ops_sec_value = extract_and_round_ops_per_sec(line)
                             if config == "isolated":
                                 workload_data[0].append(ops_sec_value)
-                            elif config == "vanilla":
+                            elif config == "Vanilla":
                                 workload_data[1].append(ops_sec_value)
                             elif config == "CIPI_PERF":
                                 workload_data[2].append(ops_sec_value)
